47106505657f0cace22a14994bd0a615320b8083
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2018-09-27 13:32:53 -0400

    added modal to ReservationDetailsModal component

--- client/src/components/ReservationDetailsModal/index.js
+++ client/src/components/ReservationDetailsModal/index.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import settings from '../../config/settings';
 import './ReservationDetailsModal.scss';
-
+import { Button, Header, Modal } from 'semantic-ui-react'
 
 class ReservationDetailsModal extends Component {
   componentDidMount() {
@@ -11,7 +11,18 @@ class ReservationDetailsModal extends Component {
   render() {
     return (
       <div id="reservation-details-modal">
-        <h1> ReservationDetailsModal </h1>
+        <Modal trigger={<Button>Show Modal</Button>} centered={false}>
+          <Modal.Header>Reservation Details</Modal.Header>
+          <Modal.Content image>
+            <Modal.Description>
+              <Header>Room #</Header>
+              <p>Date: </p>
+              <p>From:</p>
+              <p>We've found the following gravatar image associated with your e-mail address.</p>
+              <p>Is it okay to use this photo?</p>
+            </Modal.Description>
+          </Modal.Content>
+        </Modal>
       </div>
     )
   }
