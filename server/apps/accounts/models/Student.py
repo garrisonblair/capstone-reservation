b124351c398c5c6bc37e30cb764bbb8b4826cb80
Frederic Daigle <Freddydaigle@gmail.com>
2018-09-27 23:02:48 -0400

    #9 Changed student_id field to CharField

--- server/apps/accounts/models/Student.py
+++ server/apps/accounts/models/Student.py
@@ -6,7 +6,7 @@ from django.dispatch import receiver
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, default=None, null=True)
-    student_id = models.TextField(max_length=8, blank=False, primary_key=True)
+    student_id = models.CharField(max_length=8, blank=False, primary_key=True)
 
     def __str__(self):
         return self.student_id
