5ecb0d48c4314faa91bcc922ee6d1bbbe5519c41
Garrison Blair <garrisonblair96@gmail.com>
2018-09-29 14:36:36 -0400

    #9 removed is_active field as it is unnecessary and fixed str method to return a descriptive string of the object

--- server/apps/accounts/models/Room.py
+++ server/apps/accounts/models/Room.py
@@ -7,7 +7,6 @@ class Room(models.Model):
     room_id = models.CharField(max_length=50, blank=False, primary_key=True)
     capacity = models.PositiveIntegerField()
     number_of_computers = models.PositiveIntegerField()
-    is_active = models.BooleanField()
 
     def __str__(self):
-        return self
+        return '%s, Capacity: %d, Number of computers: %d' % (self.room_id, self.capacity, self.number_of_computers)
