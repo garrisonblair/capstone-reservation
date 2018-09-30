6dab04f13d65f911d9393c5913ed3904cd86919f
Frederic Daigle <Freddydaigle@gmail.com>
2018-09-30 14:49:08 -0400

    #9 added Login endpoint and view to retrieve the logged in user to test that it works.  Added test for log in

--- server/apps/accounts/urls.py
+++ server/apps/accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views.user import UserList
+from .views.me import MyUser
+from rest_framework.authtoken import views
 
 
 urlpatterns = [
-    path(r'users', UserList.as_view())
+    path(r'users', UserList.as_view()),
+    path(r'login', views.obtain_auth_token),
+    path(r'me', MyUser.as_view())
 ]
