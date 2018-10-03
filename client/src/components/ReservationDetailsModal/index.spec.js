db8f2cf29d536060dfb55a074e52682d94403003
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2018-10-03 12:33:41 -0400

    #37 test passed properties to ReservationDetailsModal

--- client/src/components/ReservationDetailsModal/index.spec.js
+++ client/src/components/ReservationDetailsModal/index.spec.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ReservationDetailsModal from '../ReservationDetailsModal';
+import { Header } from 'semantic-ui-react';
 
-it('renders without crashing', () => {
-  shallow(<ReservationDetailsModal />);
-});
+
+describe('ReservationDetailsModal', () => {
+  it('renders without crashing', () => {
+    shallow(<ReservationDetailsModal />);
+  });
+
+  it('should render this.props properly', () => {
+    var testDate = new Date().toDateString();
+    var testRoomNumber = 4;
+    const wrapper = shallow(<ReservationDetailsModal roomNumber="testRoomNumber" date="testDate" />);
+
+    // console.log(wrapper.find('Header'));
+    expect(wrapper.contains(<Header>Room # {testRoomNumber}</Header>));
+    expect(wrapper.contains(<p>Date: {testDate} </p>));
+
+  })
+})
