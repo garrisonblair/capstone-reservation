cad6aa912038ba0845942f07bba379b0837b849c
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2018-10-04 16:46:02 -0400

    #37 make use of this.props and this.state

--- client/src/components/ReservationDetailsModal/index.js
+++ client/src/components/ReservationDetailsModal/index.js
@@ -1,67 +1,48 @@
 import React, { Component } from 'react';
 import settings from '../../config/settings';
-//import './ReservationDetailsModal.scss';
+import './ReservationDetailsModal.scss';
 import { Button, Header, Modal, Dropdown } from 'semantic-ui-react';
 import axios from 'axios';
 
 class ReservationDetailsModal extends Component {
 
-  roomNumber = this.props.roomNumber;
-  date = this.props.date;
-  defaultHour = this.props.defaultHour;
-  defaultMinute = this.props.defaultMinute;
   opened = false;
-  hourOptions = this.props.hourOptions;
-  state = { modalOpen: false }
-
-  minuteOptions = [
-    { text: '00', value: 0 },
-    { text: '10', value: 10 },
-    { text: '20', value: 20 },
-    { text: '30', value: 30 },
-    { text: '40', value: 40 },
-    { text: '50', value: 50 }
-  ];
-
-  reservedOptions = [
-    // here the value should be 'this.props.username'
-    { text: 'me', value: 1 }
-  ]
-  // setHourOptions(startHour, endHour) {
-  //   for (var i = startHour; i < endHour; i++) {
-  //     this.hourOptions.push({text: i + '', value: i});
-  //     console.log(i);
-  //   }
-  // }
-
-  //This is a setup for me
-  testSetup() {
-    this.roomNumber = 23;
-    this.defaultHour = 8;
-    this.defaultMinute = 50;
-    this.date = new Date().toDateString();
-    this.hourOptions = [
-      { text: '08', value: 8 },
-      { text: '09', value: 9 },
+  state = {
+    modalOpen: false,
+    minHour: this.props.minHour || 8,
+    maxHour: this.props.maxHour || 24,
+    date: this.props.date.toDateString() || "please provide a date",
+    defaultHour: this.props.defaultHour,
+    defaultMinute: this.props.defaultMinute,
+    roomNumber: this.props.roomNumber || "please provide a room number",
+    hourOptions:[],
+    minuteOptions: [
+      { text: '00', value: 0 },
       { text: '10', value: 10 },
-      { text: '11', value: 11 },
-      { text: '12', value: 12 },
-      { text: '13', value: 13 },
-      { text: '14', value: 14 },
-      { text: '15', value: 15 },
-      { text: '16', value: 16 },
-      { text: '17', value: 17 },
-      { text: '18', value: 18 },
-      { text: '19', value: 19 },
       { text: '20', value: 20 },
-      { text: '21', value: 21 },
-      { text: '22', value: 22 },
-      { text: '23', value: 23 }
-    ];
-
+      { text: '30', value: 30 },
+      { text: '40', value: 40 },
+      { text: '50', value: 50 }
+    ],
+    reservedOptions: [
+      // here the value should be 'this.props.username'
+      { text: 'me', value: 1 }
+    ]
   }
+
+  generateHourOptions(minHour, maxHour) {
+    let result = [];
+    for (var i = minHour; i < maxHour; i++) {
+      result.push({ text: i + '', value: i });
+    }
+
+    return result;
+  }
+
   componentWillMount() {
-    this.testSetup();
+    this.setState({
+      hourOptions: this.generateHourOptions(this.state.minHour, this.state.maxHour)
+    })
   }
 
   closeModal = () => this.setState({ modalOpen: false });
@@ -77,21 +58,21 @@ class ReservationDetailsModal extends Component {
           <Modal.Header>Reservation Details</Modal.Header>
           <Modal.Content>
             <Modal.Description>
-              <Header>Room # {this.roomNumber}</Header>
-              <p>Date: {this.date} </p>
+              <Header>Room {this.state.roomNumber} </Header>
+              <p>Date: {this.state.date}</p>
               <br />
               <span>
                 <span className="inputLabel">From:</span>
-                <Dropdown placeholder='hh' selection compact className="timeSelection" options={this.hourOptions} defaultValue={this.defaultHour}/>
-                <Dropdown placeholder='mm' selection compact className="timeSelection" options={this.minuteOptions} defaultValue={this.defaultMinute} />
+                <Dropdown placeholder='hh' selection compact className="timeSelection" options={this.state.hourOptions} defaultValue={this.state.defaultHour} />
+                <Dropdown placeholder='mm' selection compact className="timeSelection" options={this.state.minuteOptions} defaultValue={this.state.defaultMinute} />
                 <span className="inputLabel" id="toLabel">To:</span>
-                <Dropdown placeholder='hh' selection compact className="timeSelection" options={this.hourOptions} />
-                <Dropdown placeholder='mm' selection compact className="timeSelection" options={this.minuteOptions} />
+                <Dropdown placeholder='hh' selection compact className="timeSelection" options={this.state.hourOptions} />
+                <Dropdown placeholder='mm' selection compact className="timeSelection" options={this.state.minuteOptions} />
               </span>
               <br /><br />
               <span>
                 <span className="inputLabel">Reserved by:</span>
-                <Dropdown compact placeholder='hh' selection options={this.reservedOptions} defaultValue={this.reservedOptions[0].value} />
+                <Dropdown compact placeholder='hh' selection options={this.state.reservedOptions} defaultValue={this.state.reservedOptions[0].value} />
               </span>
               <br /><br />
               <div>
