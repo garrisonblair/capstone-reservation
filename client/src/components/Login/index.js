4c65b1020ff0f44eed08052219c19be242e4ffbb
xTEddie <edwardtranv1@gmail.com>
2018-10-05 22:03:54 -0400

    #48 Add logic to handle login

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,10 +1,45 @@
 import React, {Component} from 'react';
-import {Button, Form, Icon, Input} from 'semantic-ui-react'
+import axios from 'axios';
+import {Button, Form, Icon, Input} from 'semantic-ui-react';
+import settings from '../../config/settings';
 import './Login.scss';
 
 
 class Login extends Component {
-  render() {
+
+  handleUsernameChange = (event) => {
+    this.setState({username: event.target.value})
+  }
+
+  handlePasswordChange = (event) => {
+    this.setState({password: event.target.value})
+  }
+
+  handleLogin = () => {
+    const {history} = this.props;
+    const {username, password} = this.state;
+    let data = {
+      username,
+      password
+    }
+    axios({
+      method: 'POST',
+      url: `${settings.API_ROOT}/login`,
+      data: data
+    })
+    .then((response) =>
+      response.data
+    )
+    .then((data) => {
+      localStorage.setItem('CapstoneReservationUser', JSON.stringify(data));
+      history.push('/');
+    })
+    .catch((error) => {
+      console.log("ERROR");
+    })
+  }
+
+  render = () => {
     return (
       <div className="login login--center">
         <div className="login__container">
@@ -15,13 +50,28 @@ class Login extends Component {
             <div className="ui divider"/>
             <div className="login__container__main__form-wrapper">
               <Form.Field>
-                <Input fluid size='small' icon='user' iconPosition='left' placeholder='Username' />
+                <Input
+                  fluid
+                  size='small'
+                  icon='user'
+                  iconPosition='left'
+                  placeholder='Username'
+                  onChange={this.handleUsernameChange}
+                />
               </Form.Field>
               <Form.Field>
-                <Input fluid size='small' icon='lock' iconPosition='left' placeholder='Password' type='password'/>
+                <Input
+                  fluid
+                  size='small'
+                  icon='lock'
+                  iconPosition='left'
+                  placeholder='Password'
+                  type='password'
+                  onChange={this.handlePasswordChange}
+                />
               </Form.Field>
               <Form.Field>
-                <Button fluid size='small' icon>
+                <Button fluid size='small' icon onClick={this.handleLogin}>
                   Login
                 </Button>
               </Form.Field>
