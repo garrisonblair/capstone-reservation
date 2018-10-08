8e516efe9c19d291658e6c521384eba1dc7c109f
xTEddie <edwardtranv1@gmail.com>
2018-10-08 19:47:02 -0400

    #32 Add sample users and students DB data & Update script to populate DB

--- server/scripts/populate_db.sh
+++ server/scripts/populate_db.sh
@@ -1,2 +1,4 @@
 python manage.py makemigrations
 python manage.py migrate
+python manage.py loaddata apps/accounts/fixtures/users.json
+python manage.py loaddata apps/accounts/fixtures/accounts.json
