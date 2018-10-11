3f9e2d1a0c14f47bd2c3a1e605a0bf4d1f1b4143
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2018-10-11 16:43:32 -0400

    #45 create Registration component

---
+++ client/src/components/Registration/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './Registration.scss';
+
+
+class Registration extends Component {
+  render() {
+    return (
+      <div id="registration">
+        <h1> Registration works!</h1>
+      </div>
+    )
+  }
+}
+
+export default Registration;
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import NotFound from './components/NotFound';
-import ReservationDetailsModal from './components/ReservationDetailsModal';
+import Registration from './components/Registration';
 
 
 ReactDOM.render(
@@ -13,10 +13,7 @@ ReactDOM.render(
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/login" component={Login}/>
-
-        {/* This Route is only for development testing. Remove after.*/}
-<Route exact path="/andresModal" render={() => <ReservationDetailsModal date ={new Date()} defaultHour={8} defaultMinute={30} roomNumber={1}/>}/>
-
+        <Route exact path="/registration" component={Registration}/>
         <Route component={NotFound}/>
       </Switch>
     </div>
