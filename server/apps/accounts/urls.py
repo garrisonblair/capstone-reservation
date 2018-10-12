7cfc92bc15a4408a8a93bc5f24cbbf33b01a8194
xTEddie <edwardtranv1@gmail.com>
2018-10-12 15:23:12 -0400

    #60 Update login endpoint for LDAP authentication

--- server/apps/accounts/urls.py
+++ server/apps/accounts/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views.user import UserList
-from .views.me import MyUser
-from rest_framework.authtoken import views
-
+from apps.accounts.views.user import UserList
+from apps.accounts.views.me import MyUser
+from apps.accounts.views.login import LoginView
 
 urlpatterns = [
     path(r'users', UserList.as_view()),
-    path(r'login', views.obtain_auth_token),
+    path(r'login', LoginView.as_view()),
     path(r'me', MyUser.as_view())
 ]
