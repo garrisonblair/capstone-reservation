93d864c81573926ac07637d6ba861c378043d213
xTEddie <edwardtranv1@gmail.com>
2018-10-12 16:40:10 -0400

    #60 Extract logic for LDAP authentication in register endpoint

--- server/apps/accounts/urls.py
+++ server/apps/accounts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from apps.accounts.views.register import RegisterView
+from apps.accounts.views.login import LoginView
 from apps.accounts.views.user import UserList
 from apps.accounts.views.me import MyUser
-from apps.accounts.views.login import LoginView
 
 urlpatterns = [
-    path(r'users', UserList.as_view()),
+    path(r'register', RegisterView.as_view()),
     path(r'login', LoginView.as_view()),
+
+    path(r'users', UserList.as_view()),
     path(r'me', MyUser.as_view())
 ]
---
+++ server/apps/accounts/views/register.py
@@ -0,0 +1,45 @@
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from utils.ldap_server import get_ldap_connection
+from apps.accounts.serializers.UserSerializer import UserSerializerLogin
+
+
+# TODO: Send email
+class RegisterView(APIView):
+    authentication_classes = ()
+    permission_classes = ()
+
+    def post(self, request):
+        """
+            Register new user.
+        """
+
+        username = request.data.get('username')
+        user = None
+
+        # Non-verified user
+        User = get_user_model()
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            pass
+
+        if user and not user.is_active:
+            print("Non-verified user")
+            return Response(status=status.HTTP_302_FOUND)
+
+        # New user from LDAP
+        connection = get_ldap_connection()
+        user = connection.get_user(username=username)
+
+        if user:
+            print("New LDAP user")
+            user.is_active = False
+            user.save()
+            # Send email
+            return Response(status=status.HTTP_201_CREATED)
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
