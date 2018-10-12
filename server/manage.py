eb160c7e5e4a79a276efdeb78369b960ba2890fa
xTEddie <edwardtranv1@gmail.com>
2018-10-12 17:37:02 -0400

    #60 Install django-dotenv & Extract environment configs to .env file

--- server/manage.py
+++ server/manage.py
@@ -1,9 +1,11 @@
 import os
 import sys
-from config.environment import SETTINGS_MODULE
+import dotenv
 
 if __name__ == '__main__':
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", SETTINGS_MODULE)
+    # Read .env file
+    dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env'))
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'config.settings.{}'.format(os.environ.get('DJANGO_ENV')))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
