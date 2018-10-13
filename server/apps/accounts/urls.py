5e85fe8148db51a0943e58c1973e2f6538f1e66a
xTEddie <edwardtranv1@gmail.com>
2018-10-13 04:23:44 -0400

    #60 Add logout endpoint to delete API token

--- server/apps/accounts/urls.py
+++ server/apps/accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from apps.accounts.views.register import RegisterView
 from apps.accounts.views.verify import VerifyView
 from apps.accounts.views.login import LoginView
+from apps.accounts.views.logout import LogoutView
 from apps.accounts.views.user import UserList
 from apps.accounts.views.me import MyUser
 
@@ -9,6 +10,7 @@ urlpatterns = [
     path(r'register', RegisterView.as_view()),
     path(r'verify', VerifyView.as_view()),
     path(r'login', LoginView.as_view()),
+    path(r'logout', LogoutView.as_view()),
 
     path(r'users', UserList.as_view()),
     path(r'me', MyUser.as_view())
