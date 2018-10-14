cef1b03b64d2a4f79a3e5935e38768370b5dcc33
xTEddie <edwardtranv1@gmail.com>
2018-10-14 01:38:24 -0400

    #60 Add check if .env file exists

--- server/config/wsgi.py
+++ server/config/wsgi.py
@@ -4,7 +4,9 @@ import dotenv
 from django.core.wsgi import get_wsgi_application
 
 # Read .env file
-dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), '.env'))
+env_file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
+if os.path.exists(env_file_path):
+    dotenv.read_dotenv(env_file_path)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'config.settings.{}'.format(os.environ.get('DJANGO_ENV')))
 
 application = get_wsgi_application()
--- server/manage.py
+++ server/manage.py
@@ -4,7 +4,9 @@ import dotenv
 
 if __name__ == '__main__':
     # Read .env file
-    dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env'))
+    env_file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
+    if os.path.exists(env_file_path):
+        dotenv.read_dotenv(env_file_path)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'config.settings.{}'.format(os.environ.get('DJANGO_ENV')))
     try:
         from django.core.management import execute_from_command_line
