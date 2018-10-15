0f8903228ebaac1246df15c15e7274cc50492ce0
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2018-10-15 13:38:27 -0400

    #45 add link to Registration from Login && add Registration form

--- client/src/components/Registration/index.js
+++ client/src/components/Registration/index.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Registration.scss';
-
+import {Form, Input, Button} from 'semantic-ui-react';
 
 class Registration extends Component {
+
+  handleEncsUsername = () => {
+
+  }
+
   render() {
     return (
       <div id="registration">
         <div className="container">
-        <h1>Registration</h1>
+          <h1>Registration</h1>
+          <p>Please enter your ENCS username. A validation will be sent to your email.</p>
+          <Form.Field>
+            <Input
+              fluid
+              size='small'
+              icon='user'
+              iconPosition='left'
+              placeholder='ex. a_test'
+              onChange={this.handleEncsUsername}
+            />
+          </Form.Field>
+          <Form.Field>
+            <br />
+            <Button fluid size='small' icon onClick={this.handleLogin}>
+              Send Email
+            </Button>
+          </Form.Field>
         </div>
       </div>
     )
