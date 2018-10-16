8647d3610ff06157e5243bfc157eab0ee94f5cf7
Frederic Daigle <Freddydaigle@gmail.com>
2018-10-15 21:25:18 -0400

    #46 added minimum commit message length

--- scripts/commit-msg.py
+++ scripts/commit-msg.py
@@ -7,21 +7,23 @@ MERGE_REGEX = "Merge"
 
 def validate_commit_message():
     """ Make sure commit message has is linked to an issue. """
-
+    messabe_is_valid = 0
     try:
         script, commit_msg = sys.argv
 
         link_match = re.search(LINK_REGEX, commit_msg)
         merge_match = re.search(MERGE_REGEX, commit_msg, re.IGNORECASE)
 
-        if link_match is not None or merge_match is not None:
-            return 0
+        if link_match is None or merge_match is None:
+            print("Commit message does not link to an issue and isn't a merge commit")
+            return 1
     except Exception as e:
         print("ERROR")
         print(e)
 
-    print("Commit message does not link to an issue and isn't a merge commit")
-    return 1
+    if len(commit_msg) <= 5:
+        print("Commit message too short")
+        return 1
 
 
 if __name__ == '__main__':
--- scripts/setup_hooks.sh
+++ scripts/setup_hooks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cp scripts/commit-msg .git/hooks/
-cp scripts/pre-commit.sh .git/hooks/
+cp scripts/pre-commit .git/hooks/
 
 chmod +x .git/hooks/commit-msg
-chmod +x .git/hooks/pre-commit.sh
+chmod +x .git/hooks/pre-commit
