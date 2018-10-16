36f10828747844101ca66a47bc25b7c33f4d29c1
Frederic Daigle <Freddydaigle@gmail.com>
2018-10-15 21:26:23 -0400

    #46 moved min length to static var

--- scripts/commit-msg.py
+++ scripts/commit-msg.py
@@ -4,6 +4,7 @@ import sys
 LINK_REGEX = "#[0-9]+"
 MERGE_REGEX = "Merge"
 
+COMMIT_MESSAGE_LENGTH = 6
 
 def validate_commit_message():
     """ Make sure commit message has is linked to an issue. """
@@ -14,14 +15,14 @@ def validate_commit_message():
         link_match = re.search(LINK_REGEX, commit_msg)
         merge_match = re.search(MERGE_REGEX, commit_msg, re.IGNORECASE)
 
-        if link_match is None or merge_match is None:
+        if link_match is None and merge_match is None:
             print("Commit message does not link to an issue and isn't a merge commit")
             return 1
     except Exception as e:
         print("ERROR")
         print(e)
 
-    if len(commit_msg) <= 5:
+    if len(commit_msg) <= COMMIT_MESSAGE_LENGTH:
         print("Commit message too short")
         return 1
 
