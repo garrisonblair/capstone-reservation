8e5a37feea86f0de9b5d5e918a4ed3524c6a7430
kammyWong <kammy609@msn.com>
2018-10-26 21:43:38 -0400

    #14 Add more restriction for CampOn

--- server/apps/booking/models/CampOn.py
+++ server/apps/booking/models/CampOn.py
@@ -25,9 +25,26 @@ class CampOn(models.Model):
 
     def validate_model(self):
         today = datetime.now().strftime("%Y-%m-%d")
+        invalid_start_time = datetime.strftime("8:00", "%H:%M").time()
+        invalid_end_time = datetime.strftime("23:00", "%H:%M").time()
+
         if (str(self.booking.date) != today):
             raise ValidationError("Camp-on can only be done for today.")
+
+        elif (self.start_time < self.booking.start_time):
+            raise ValidationError("Start time has to be in between the selected Booking period.")
+
+        elif (self.start_time >= self.booking.end_time):
+            raise ValidationError("Start time has to be in between the selected Booking period.")
+
+        elif (self.end_time < invalid_start_time):
+            raise ValidationError("End time cannot be earlier than 8:00.")
+
+        elif (self.end_time > invalid_end_time):
+            raise ValidationError("End time cannot be later than 23:00.")
+
         elif self.start_time >= self.end_time:
             raise ValidationError("End time must be later than the start time")
+
         elif CampOn.objects.filter(student=self.student, booking=self.booking).exists():
             raise ValidationError("Cannot camp-on the same Booking.")
