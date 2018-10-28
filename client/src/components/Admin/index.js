b59f9b50bf9c78797277673d095d8c2283980184
kemoua <kemoua@gmail.com>
2018-10-28 01:43:27 -0400

    #63 Handling get and patch requests

--- client/src/components/Admin/index.js
+++ client/src/components/Admin/index.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import settings from '../../config/settings';
 import axios from 'axios';
 import './Admin.scss';
+import { Button } from 'semantic-ui-react';
+import {getTokenHeader} from '../../utils/requestHeaders';
 
 
 class Admin extends Component {
@@ -9,38 +11,67 @@ class Admin extends Component {
   state = {
     isAdmin: false
   }
+  sweetAlert = require('sweetalert2');
 
   componentDidMount = () => {
     document.title = 'Capstone Settings'
     if(localStorage.getItem('CapstoneReservationUser')) {
       let user = JSON.parse(localStorage.getItem('CapstoneReservationUser'));
       if(user.is_superuser) {
+        this.getSettings()
         this.setState({
           isAdmin: true,
           current: 'Settings'
         })
-        this.getSettings()
       }
     }
   }
 
   /************ REQUESTS *************/
 
-  //TODO: Put correct url and handle response
   getSettings() {
-    // axios({
-    //   method: 'GET',
-    //   url: `${settings.API_ROOT}/settings`
-    // })
-    // .then((response) => {
+    axios({
+      method: 'GET',
+      url: `${settings.API_ROOT}/settings`
+    })
+    .then((response) => {
+      console.log(response.data)
+      this.setState({
+        webcalendar_backup: response.data.is_webcalendar_backup_active
+      })
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+  }
 
-    // })
-    // .catch(function (error) {
-    //   console.log(error);
-    // })
-    // .then(function () {
-    //   // always executed
-    // });
+  saveSettings = () => {
+    console.log(this.state.webcalendar_backup)
+    const headers = getTokenHeader();
+    let data = {
+      is_webcalendar_backup_active: this.state.webcalendar_backup ? 'true' : 'false'
+    }
+    axios({
+      method: 'PATCH',
+      url: `${settings.API_ROOT}/settings`,
+      data,
+      headers,
+      withCredentials: true,
+    })
+    .then((response) => {
+      this.sweetAlert('Completed',
+          `Settings were successfuly saved.`,
+          'success')
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
   }
 
   /************ CLICK HANDLING METHODS *************/
@@ -50,19 +81,24 @@ class Admin extends Component {
     this.setState({current: option})
   }
 
+  handleChangeSetting = (e) => {
+    let setting = e.target.getAttribute('value');
+    this.setState({[setting]: !this.state[setting]})
+    console.log(this.state)
+  }
 
   /************ COMPONENT RENDERING *************/
 
   renderSettings() {
     if(this.state.isAdmin) {
-      return <div className="admin__wrapper"> <div>{this.renderNav()}</div><div>{this.renderContent()}</div></div>
+      return <div className="admin__wrapper"> <div>{this.renderNav()}</div><div className="admin__content">{this.renderContent()}</div></div>
     } else {
-      return <div> NOT FOUNDED </div>
+      return <div> NOT FOUND </div>
     }
   }
 
   renderNav() {
-    const options = ['Settings', 'Web Calendar', 'Stats']
+    const options = ['Settings', 'Stats']
     const menu = options.map((option) => 
       <li className={this.state.current == option ? "active" : ""} key={option} value={option} onClick={this.handleClickNav}>{option}</li>
     )
@@ -72,15 +108,27 @@ class Admin extends Component {
   renderContent() {
     const {current} = this.state
     let content
-    if(current == "Settings") {
-      content = <div>Settings</div>
-    } else if(current == "Web Calendar") {
-      content = <div>Web Calendar</div>
-    } else {
-      content = <div>Stats</div>
-    } 
-    return content
-    
+    switch (current) {
+      case "Settings": 
+        content = this.renderContentSettings()
+        return content
+      case "Stats":
+        content = <div>Stats Content</div>;
+        return content
+    }
+  }
+
+  renderContentSettings() {
+    return (
+      <form onSubmit={this.saveSettings}>
+        <label>
+          Automatically export to Web Calendar
+          <input type="checkbox" checked={!!this.state.webcalendar_backup} value="webcalendar_backup" onChange={this.handleChangeSetting} />
+        </label>
+        <br/>
+        <input type="submit" value="Save" />
+      </form>
+    )
   }
   
 
