26b67a36a3d014b7ea849ee6bc2b680b8a29b57a
kemoua <kemoua@gmail.com>
2018-10-28 16:28:40 -0400

    #63 Added webcalendar credentials handling

--- client/src/components/Admin/index.js
+++ client/src/components/Admin/index.js
@@ -2,14 +2,18 @@ import React, {Component} from 'react';
 import settings from '../../config/settings';
 import axios from 'axios';
 import './Admin.scss';
-import { Button } from 'semantic-ui-react';
+import SweetAlert from 'sweetalert2-react';
+import {Button, Form, Modal, Input, Header} from 'semantic-ui-react';
 import {getTokenHeader} from '../../utils/requestHeaders';
 
 
 class Admin extends Component {
 
   state = {
-    isAdmin: false
+    isAdmin: false,
+    loginModal: false,
+    webcalendarUsername: '',
+    webcalendarPassword: ''
   }
   sweetAlert = require('sweetalert2');
 
@@ -48,9 +52,13 @@ class Admin extends Component {
   }
 
   saveSettings = () => {
+    const {webcalendarPassword, webcalendarUsername} = this.state
     const headers = getTokenHeader();
     let data = {
-      is_webcalendar_backup_active: this.state.webcalendar_backup ? 'true' : 'false'
+      is_webcalendar_backup_active: this.state.webcalendar_backup ? 'true' : 'false',
+      webcalendar_username: webcalendarUsername,
+      webcalendar_password: webcalendarPassword
+
     }
     axios({
       method: 'PATCH',
@@ -63,9 +71,15 @@ class Admin extends Component {
       this.sweetAlert('Completed',
           `Settings were successfuly saved.`,
           'success')
+      this.toggleLoginModal();
     })
     .catch(function (error) {
       console.log(error);
+      if (error.message.includes('401')) {
+        this.sweetAlert('Autenthication error',
+          'The credentials you entered are invalid.',
+          'error')
+      }
     })
     .then(function () {
       // always executed
@@ -82,7 +96,29 @@ class Admin extends Component {
   handleChangeSetting = (e) => {
     let setting = e.target.getAttribute('value');
     this.setState({[setting]: !this.state[setting]})
+    if(setting = "webcalendar_backup") {
+      this.toggleLoginModal();
+    }
   }
+
+  handleWebCalendarPasswordChange = (e) => {
+    this.setState({webcalendarPassword: e.target.value})
+  }
+
+  handleWebCalendarUsernameChange = (e) => {
+    this.setState({webcalendarUsername: e.target.value})
+  }
+
+  /************ HELPER METHOD *************/
+
+  toggleLoginModal = () => {
+    this.setState({loginModal: !this.state.loginModal})
+  }
+
+  closeResponseModal = () => {
+    this.setState({responseModal: !this.state.responseModal})
+  }
+
 
   /************ COMPONENT RENDERING *************/
 
@@ -123,16 +159,60 @@ class Admin extends Component {
           <input type="checkbox" checked={!!this.state.webcalendar_backup} value="webcalendar_backup" onChange={this.handleChangeSetting} />
         </label>
         <br/>
-        <input type="submit" value="Save" />
+        {/* <input type="submit" value="Save" /> */}
       </form>
+    )
+  }
+
+  renderLoginModal() {
+    return (
+      <Modal open={this.state.loginModal} onClose={this.toggleLoginModal}>
+          <Header>
+            <h1 className="login__container__header__title"> Please enter your Webcalendar credentials </h1>
+          </Header>
+          <div className="login__container__main">
+            <div className="ui divider"/>
+            <div className="login__container__main__form-wrapper">
+              <Form.Field>
+                <Input
+                  fluid
+                  size='small'
+                  icon='user'
+                  iconPosition='left'
+                  placeholder='Username'
+                  onChange={this.handleWebCalendarUsernameChange}
+                />
+              </Form.Field>
+              <Form.Field>
+                <Input
+                  fluid
+                  size='small'
+                  icon='lock'
+                  iconPosition='left'
+                  placeholder='Password'
+                  type='password'
+                  onChange={this.handleWebCalendarPasswordChange}
+                />
+              </Form.Field>
+              <Form.Field>
+                <Button fluid size='small' icon onClick={this.saveSettings}>
+                  Save
+                </Button>
+              </Form.Field>
+              <div className="ui divider"/>
+            </div>
+          </div>
+        </Modal>
     )
   }
   
 
   render() {
+    const {responseModal, responseModalText, responseModalTitle, responseModalType} = this.state
     return (
       <div>
         {this.renderSettings()}
+        {this.renderLoginModal()}
       </div>     
     )
   }
