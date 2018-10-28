347bd4564d904336c910b1d3f3149abd8e872146
SteveLocke <tap3stry@gmail.com>
2018-10-28 14:30:05 -0400

    #46 Initial skeleton of ICS serializer and empty test class

--- server/apps/booking_exporter/WEBCalendarExporter/ICSSerializer.py
+++ server/apps/booking_exporter/WEBCalendarExporter/ICSSerializer.py
@@ -3,4 +3,33 @@
 class ICSSerializer:
 
     def serialize_booking(self, booking):
-        pass
+
+        #booking.student
+        #booking.student_group (conditionally)
+        summary = ""
+
+        print('booking.student: ', booking.student)
+        print('booking.student_group: ', booking.student_group)
+        print('booking.start_time: ', booking.start_time)
+        print('booking.end_time: ', booking.end_time)
+
+        # DTSTART:20181012T103000
+        # DTEND:20181012T120000
+
+        ics_file = """"BEGIN:VCALENDAR
+        METHOD:PUBLISH
+        BEGIN:VEVENT
+        UID:{}
+        SUMMARY:{}
+        DESCRIPTION:{}
+        CLASS:PUBLIC
+        STATUS:TENTATIVE
+        DTSTART:{}
+        DTEND:{}
+        END:VEVENT
+        END:VCALENDAR"""
+
+#        ics_file.format(booking.id, 1,1, , )
+        return ics_file
+
+
---
+++ server/apps/booking_exporter/tests/testICSSerializer.py
@@ -0,0 +1,10 @@
+
+import datetime
+
+from django.test import TestCase
+from unittest import mock
+
+from apps.booking.models import Booking
+from apps.rooms.models import Room
+from apps.accounts.models import Student
+
