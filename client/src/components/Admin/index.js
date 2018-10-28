3da532110de4fab06ad99d03c9b2a9eac0313705
kemoua <kemoua@gmail.com>
2018-10-27 22:00:36 -0400

    #63 Added request placeholder and navigation bar

--- client/src/components/Admin/index.js
+++ client/src/components/Admin/index.js
@@ -15,13 +15,17 @@ class Admin extends Component {
     if(localStorage.getItem('CapstoneReservationUser')) {
       let user = JSON.parse(localStorage.getItem('CapstoneReservationUser'));
       if(user.is_superuser) {
-        this.setState({isAdmin: true})
+        this.setState({
+          isAdmin: true,
+          current: 'Settings'
+        })
         this.getSettings()
       }
     }
   }
 
   /************ REQUESTS *************/
+
   //TODO: Put correct url and handle response
   getSettings() {
     // axios({
@@ -39,15 +43,46 @@ class Admin extends Component {
     // });
   }
 
+  /************ CLICK HANDLING METHODS *************/
+
+  handleClickNav = (e) => {
+    let option = e.target.getAttribute('value');
+    this.setState({current: option})
+  }
+
 
   /************ COMPONENT RENDERING *************/
+
   renderSettings() {
     if(this.state.isAdmin) {
-      return <div> ADMIN </div>
+      return <div className="admin__wrapper"> <div>{this.renderNav()}</div><div>{this.renderContent()}</div></div>
     } else {
-      return <div> NOT ADMIN </div>
+      return <div> NOT FOUNDED </div>
     }
   }
+
+  renderNav() {
+    const options = ['Settings', 'Web Calendar', 'Stats']
+    const menu = options.map((option) => 
+      <li className={this.state.current == option ? "active" : ""} key={option} value={option} onClick={this.handleClickNav}>{option}</li>
+    )
+    return <ul className="admin__navigation">{menu}</ul>
+  }
+
+  renderContent() {
+    const {current} = this.state
+    let content
+    if(current == "Settings") {
+      content = <div>Settings</div>
+    } else if(current == "Web Calendar") {
+      content = <div>Web Calendar</div>
+    } else {
+      content = <div>Stats</div>
+    } 
+    return content
+    
+  }
+  
 
   render() {
     return (
