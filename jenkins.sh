79a9d40ea84d4f8c45d291fae972a5d17cdb2df9
SteveLocke <tap3stry@gmail.com>
2018-10-28 22:21:42 -0400

    #46 Attempting to add code coverage to pipeline

--- jenkins.sh
+++ jenkins.sh
@@ -32,10 +32,10 @@ set -x
 function staticAnalysis() {
 	echo 'Running static analysis... from jenkins.sh'
 	pycodestyle --exclude='**/migrations/**, **/__init__.py' --config='./../../setup.cfg' server
-	#source activate ${BUILD_TAG}
-    #coverage run irisvmpy/iris.py 1 1 2 3
-    #python -m coverage xml -o ./reports/coverage.xml
-    #tox -e coverage
+	source activate ${BUILD_TAG}
+    coverage run irisvmpy/iris.py 1 1 2 3
+    python -m coverage xml -o ./reports/coverage.xml
+    tox -e coverage
 }
 
 
