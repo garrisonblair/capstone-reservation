0680b23c7b179c93b93298dfc9099c0013454b77
xTEddie <edwardtranv1@gmail.com>
2018-10-31 15:58:45 -0400

    #45 Add api.js

---
+++ client/src/utils/api.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import settings from '../config/settings';
+import {getTokenHeader} from './requestHeaders';
+
+
+function login(username, password) {
+    let data = {
+        username,
+        password
+    }
+    return axios({
+        method: 'POST',
+        url: `${settings.API_ROOT}/login`,
+        data: data
+    })
+}
+
+function verify(token) {
+    const data = {
+        'token': `${token}`
+    }
+    return axios({
+        method: 'POST',
+        url: `${settings.API_ROOT}/verify`,
+        data: data
+    })
+}
+
+function updateUser(id, data) {
+    const headers = getTokenHeader();
+    return axios({
+        method: 'PATCH',
+        url: `${settings.API_ROOT}/user/${id}`,
+        headers,
+        data
+    })
+}
+
+const api = {
+    login,
+    verify,
+    updateUser
+}
+
+export default api;
