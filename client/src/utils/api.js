10ff1cb028e59b41cc70ec8c970d1a5fa969727d
xTEddie <edwardtranv1@gmail.com>
2018-10-31 20:53:40 -0400

    #45 Extract axios call for register to api class

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,43 +3,55 @@ import settings from '../config/settings';
 import {getTokenHeader} from './requestHeaders';
 
 
+function register(username) {
+  let data = {
+    'username': `${username}`
+  };
+
+  return axios({
+    method: 'POST',
+    url: `${settings.API_ROOT}/register`,
+    data: data
+  })
+}
+
 function login(username, password) {
-    let data = {
-        username,
-        password
-    }
-    return axios({
-        method: 'POST',
-        url: `${settings.API_ROOT}/login`,
-        data: data
-    })
+  let data = {
+    username,
+    password
+  }
+  return axios({
+    method: 'POST',
+    url: `${settings.API_ROOT}/login`,
+    data: data
+  })
 }
 
 function verify(token) {
-    const data = {
-        'token': `${token}`
-    }
-    return axios({
-        method: 'POST',
-        url: `${settings.API_ROOT}/verify`,
-        data: data
-    })
+  const data = {
+    'token': `${token}`
+  }
+  return axios({
+    method: 'POST',
+    url: `${settings.API_ROOT}/verify`,
+    data: data
+  })
 }
 
 function updateUser(id, data) {
-    const headers = getTokenHeader();
-    return axios({
-        method: 'PATCH',
-        url: `${settings.API_ROOT}/user/${id}`,
-        headers,
-        data
-    })
+  const headers = getTokenHeader();
+  return axios({
+    method: 'PATCH',
+    url: `${settings.API_ROOT}/user/${id}`,
+    headers,
+    data
+  })
 }
 
 const api = {
-    login,
-    verify,
-    updateUser
+  login,
+  verify,
+  updateUser
 }
 
 export default api;
