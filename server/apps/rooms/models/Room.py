5c480babaae0872f0db6bac61cb0db039906edda
steve locke <tap3stry@gmail.com>
2018-11-02 00:28:17 -0400

    #22 #21 cleaned up pep8 violations

--- server/apps/rooms/models/Room.py
+++ server/apps/rooms/models/Room.py
@@ -1,15 +1,14 @@
 from django.db import models
 
-from django.db.models import Q
+#from django.db.models import Q
 
 # Add room #21
 # Modify Room #22
 # Delete Room #23
 
 
-
-
 class RoomManager(models.Manager):
+
     def create_room(self, room_id, capacity, number_of_computers):
         room = self.create(
             room_id=room_id,
@@ -19,18 +18,13 @@ class RoomManager(models.Manager):
 
         return room
 
+
 class Room(models.Model):
     room_id = models.CharField(max_length=50, blank=False, unique=True)
     capacity = models.PositiveIntegerField(blank=True, null=True)
     number_of_computers = models.PositiveIntegerField(blank=True, null=True)
 
-    def __str__(self):
-        return '{}, Capacity: {}, Number of computers: {}'.format(
-            self.room_id, self.capacity, self.number_of_computers
-        )
-
     def save(self):
-        self.validate_model()
         is_create = False
         if self.id is None:
             is_create = True
@@ -56,4 +50,7 @@ class Room(models.Model):
         self.number_of_computers = number_of_computers
         self.save()
 
-
+    def __str__(self):
+        return '{}, Capacity: {}, Number of computers: {}'.format(
+            self.room_id, self.capacity, self.number_of_computers
+        )
