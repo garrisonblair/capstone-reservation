6fbc8c40cccac0517d001609e9f78f8758e60d67
Garrison Blair <garrisonblair96@gmail.com>
2018-11-04 00:53:00 -0400

    #18 Created serializer for privilege categories

---
+++ server/apps/accounts/serializers/privilege_category_serializer.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from ..models.PrivilegeCategory import PrivilegeCategory
+
+
+class PrivilegeCategorySerializer(serializers.ModelSerializer):
+    name = serializers.CharField()
+    parent_category = serializers.PrimaryKeyRelatedField(queryset=PrivilegeCategory.objects.all())
+    num_days_to_booking = serializers.IntegerField(allow_null=True)
+    can_make_recurring_booking = serializers.BooleanField(allow_null=True)
+    max_num_bookings = serializers.IntegerField(allow_null=True)
+    max_num_recurring_bookings = serializers.IntegerField(allow_null=True)
+
+    def create(self, validated_data):
+        return PrivilegeCategory.objects.create(
+            name=validated_data["name"],
+            parent_category=validated_data["parent_category"],
+            num_days_to_booking=validated_data["num_days_to_booking"],
+            max_num_bookings=validated_data["max_num_bookings"],
+            max_num_recurring_bookings=validated_data["max_num_recurring_bookings"]
+        )
+
+    class Meta:
+        model = PrivilegeCategory
+        fields = '__all__'
