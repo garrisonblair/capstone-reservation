1e98128417dbcd73dbb05533a682908caf86317b
xTEddie <edwardtranv1@gmail.com>
2018-11-08 18:03:50 -0500

    #18 Add new permission class to check if user is a superuser & fix test

--- server/apps/accounts/views/privilege_categories.py
+++ server/apps/accounts/views/privilege_categories.py
@@ -6,13 +6,30 @@ from rest_framework.decorators import permission_classes
 
 from django.core.exceptions import ValidationError
 
-from apps.accounts.permissions.IsOwnerOrAdmin import IsOwnerOrAdmin
+from apps.accounts.permissions.IsSuperUser import IsSuperUser
 from apps.accounts.serializers.privilege_category_serializer import PrivilegeCategorySerializer
 from apps.accounts.models.PrivilegeCategory import PrivilegeCategory
 
 
-@permission_classes((IsAuthenticated, IsOwnerOrAdmin))
+@permission_classes((IsAuthenticated, IsSuperUser))
 class PrivilegeCategoryView(APIView):
+
+    def get(self, request):
+        request_name = request.query_params.get('name', None)
+        if request_name is not None:
+            categories = PrivilegeCategory.objects.filter(name=request_name)
+            if categories.count() == 0:
+                return Response("Category named {} does not exist".format(request_name),
+                                status=status.HTTP_404_NOT_FOUND)
+        else:
+            categories = PrivilegeCategory.objects.all()
+
+        category_list = list()
+        for category in categories:
+            serializer = PrivilegeCategorySerializer(category)
+            category_list.append(serializer.data)
+        return Response(category_list, status=status.HTTP_200_OK)
+
     def post(self, request):
         # Must be logged in as admin
         # if not request.user.is_superuser:
@@ -29,24 +46,3 @@ class PrivilegeCategoryView(APIView):
                 return Response(PrivilegeCategorySerializer(category).data, status=status.HTTP_201_CREATED)
             except ValidationError as error:
                 return Response(error.messages, status=status.HTTP_400_BAD_REQUEST)
-
-    def get(self, request):
-        # Must be logged in as admin
-        # if not request.user.is_superuser:
-        #     return Response(status=status.HTTP_401_UNAUTHORIZED)
-
-        request_name = request.query_params.get('name', None)
-
-        if request_name is not None:
-            categories = PrivilegeCategory.objects.filter(name=request_name)
-            if categories.count() == 0:
-                return Response("Category named {} does not exist".format(request_name),
-                                status=status.HTTP_404_NOT_FOUND)
-        else:
-            categories = PrivilegeCategory.objects.all()
-
-        category_list = list()
-        for category in categories:
-            serializer = PrivilegeCategorySerializer(category)
-            category_list.append(serializer.data)
-        return Response(category_list, status=status.HTTP_200_OK)
