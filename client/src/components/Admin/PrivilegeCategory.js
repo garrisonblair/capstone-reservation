61865402ede378b395cb15c5b3b14f92fe51e09a
xTEddie <edwardtranv1@gmail.com>
2018-11-08 03:43:41 -0500

    #17 Add privilege controls & Update styling

--- client/src/components/Admin/PrivilegeCategory.js
+++ client/src/components/Admin/PrivilegeCategory.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import {Icon, Label, Menu, Table} from 'semantic-ui-react'
+import {Button, Icon, Table} from 'semantic-ui-react'
 import api from '../../utils/api';
 import AdminRequired from '../HOC/AdminRequired';
 import SideNav from './SideNav';
 import './Admin.scss';
 
 
+// TODO: Pagination
 class PrivilegeCategory extends Component {
 
   state = {
@@ -25,34 +26,47 @@ class PrivilegeCategory extends Component {
     })
   }
 
+  renderControls() {
+    return (
+      <div>
+        <Button icon labelPosition='left' primary size='small'>
+          <Icon name='plus'/> Add
+        </Button>
+        <Button icon labelPosition='left' primary size='small'>
+          <Icon name='plus'/> Assign
+        </Button>
+      </div>
+    )
+  }
+
   renderTable() {
     const {privileges} = this.state;
     let headers = [
       'Name',
-      'Parent',
+      'Parent Category',
       'Max Days Until Booking',
       'Max Bookings',
-      'Can Make Recurring Booking',
+      'Recurring Booking Permission',
       'Max Recurring Bookings',
-      'Booking Start Time',
-      'Booking End Time'
+      'Booking Valid Time'
     ]
 
-    let body = privileges.map((privilege) => {
-      console.log(privilege);
-      return (
-        <Table.Row key={privilege.id}>
-          <Table.Cell>{privilege.name}</Table.Cell>
-          <Table.Cell>{privilege.parent_category || '-'}</Table.Cell>
-          <Table.Cell>{privilege.max_days_until_booking}</Table.Cell>
-          <Table.Cell>{privilege.max_bookings}</Table.Cell>
-          <Table.Cell>{privilege.can_make_recurring_booking? 'Yes':'No'}</Table.Cell>
-          <Table.Cell>{privilege.max_recurring_bookings}</Table.Cell>
-          <Table.Cell>{privilege.booking_start_time}</Table.Cell>
-          <Table.Cell>{privilege.booking_end_time}</Table.Cell>
-        </Table.Row>
-      )
-    })
+    let body = privileges.map((privilege) =>
+      <Table.Row key={privilege.id}>
+        <Table.Cell>{privilege.name}</Table.Cell>
+        <Table.Cell>{privilege.parent_category || '-'}</Table.Cell>
+        <Table.Cell>{privilege.max_days_until_booking}</Table.Cell>
+        <Table.Cell>{privilege.max_bookings}</Table.Cell>
+        <Table.Cell>
+          <Icon
+            name={privilege.can_make_recurring_booking? 'check circle': 'times circle'}
+            color={privilege.can_make_recurring_booking? 'green': 'red'}
+          />
+        </Table.Cell>
+        <Table.Cell>{privilege.max_recurring_bookings}</Table.Cell>
+        <Table.Cell>{`${privilege.booking_start_time} - ${privilege.booking_end_time}`}</Table.Cell>
+      </Table.Row>
+    )
 
     let table = (
       <Table celled>
@@ -85,7 +99,8 @@ class PrivilegeCategory extends Component {
         <div className="admin__wrapper">
           <SideNav selectedMenu={'privileges'}/>
           <div className="admin__content">
-            <div>Privileges</div>
+            <h1>Privileges</h1>
+            {this.renderControls()}
             {this.renderTable()}
           </div>
         </div>
