f64cf4fb57d539fe9f9b40e77896456b18cb1259
xTEddie <edwardtranv1@gmail.com>
2018-11-08 03:08:41 -0500

    #17 Add table to display privilege categories

--- client/src/components/Admin/PrivilegeCategory.js
+++ client/src/components/Admin/PrivilegeCategory.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Icon, Label, Menu, Table} from 'semantic-ui-react'
 import api from '../../utils/api';
 import AdminRequired from '../HOC/AdminRequired';
 import SideNav from './SideNav';
@@ -7,14 +8,71 @@ import './Admin.scss';
 
 class PrivilegeCategory extends Component {
 
-  getPrivileges() {
+  state = {
+    privileges: []
+  }
+
+  getPrivileges = () => {
     api.getPrivileges()
-    .then((response) => {
-      console.log(response);
+    .then((response) => response.data)
+    .then((privileges) => {
+      this.setState({
+        privileges
+      })
     })
     .catch((error) => {
       console.log(error);
     })
+  }
+
+  renderTable() {
+    const {privileges} = this.state;
+    let headers = [
+      'Name',
+      'Parent',
+      'Max Days Until Booking',
+      'Max Bookings',
+      'Can Make Recurring Booking',
+      'Max Recurring Bookings',
+      'Booking Start Time',
+      'Booking End Time'
+    ]
+
+    let body = privileges.map((privilege) => {
+      console.log(privilege);
+      return (
+        <Table.Row key={privilege.id}>
+          <Table.Cell>{privilege.name}</Table.Cell>
+          <Table.Cell>{privilege.parent_category || '-'}</Table.Cell>
+          <Table.Cell>{privilege.max_days_until_booking}</Table.Cell>
+          <Table.Cell>{privilege.max_bookings}</Table.Cell>
+          <Table.Cell>{privilege.can_make_recurring_booking? 'Yes':'No'}</Table.Cell>
+          <Table.Cell>{privilege.max_recurring_bookings}</Table.Cell>
+          <Table.Cell>{privilege.booking_start_time}</Table.Cell>
+          <Table.Cell>{privilege.booking_end_time}</Table.Cell>
+        </Table.Row>
+      )
+    })
+
+    let table = (
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            {
+              headers.map((header, index) =>
+                <Table.HeaderCell key={index}>
+                  {header}
+                </Table.HeaderCell>
+              )
+            }
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {body}
+        </Table.Body>
+      </Table>
+    )
+    return table;
   }
 
   componentDidMount() {
@@ -28,6 +86,7 @@ class PrivilegeCategory extends Component {
           <SideNav selectedMenu={'privileges'}/>
           <div className="admin__content">
             <div>Privileges</div>
+            {this.renderTable()}
           </div>
         </div>
       </div>
