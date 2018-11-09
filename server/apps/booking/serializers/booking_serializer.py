e4c94fd4761ac2abcf8a7f82e53a61d6d15db7c5
Frederic Daigle <Freddydaigle@gmail.com>
2018-11-08 23:12:25 -0500

    #9 exposing more information on Booker in the BookingSerializer

--- server/apps/booking/serializers/booking_serializer.py
+++ server/apps/booking/serializers/booking_serializer.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from ..models.Booking import Booking
+from apps.accounts.serializers.UserSerializer import BookerSerializer
 
 
 class BookingSerializer(serializers.ModelSerializer):
+
+    booker = BookerSerializer()
+
     class Meta:
         model = Booking
         fields = ('id', 'booker', 'group', 'room', 'date', 'start_time', 'end_time')
