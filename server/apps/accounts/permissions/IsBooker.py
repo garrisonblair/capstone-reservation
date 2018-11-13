a06379a426e6684ffa4733c2d87cc0d6ca426625
xTEddie <edwardtranv1@gmail.com>
2018-11-13 05:08:45 -0500

    Fix groups API permissions

--- server/apps/accounts/permissions/IsBooker.py
+++ server/apps/accounts/permissions/IsBooker.py
@@ -4,6 +4,7 @@ from apps.accounts.models.Booker import Booker
 
 class IsBooker(BasePermission):
     def has_permission(self, request, view):
+        # Admin can overwrite permission
         if request.user.is_superuser:
             return True
 
@@ -14,6 +15,7 @@ class IsBooker(BasePermission):
         return True
 
     def has_object_permission(self, request, view, user):
+        # Admin can overwrite permission
         if request.user.is_superuser:
             return True
 
--- server/apps/groups/urls.py
+++ server/apps/groups/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from .views.groups import GroupList
+
+
+urlpatterns = [
+    path(r'groups', GroupList.as_view())
+]
