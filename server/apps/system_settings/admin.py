bfc3ea4b756a6dc0a2ccd0b6374043c9306074a1
xTEddie <edwardtranv1@gmail.com>
2018-11-13 15:55:04 -0500

    Add admin model for system_settings

--- server/apps/system_settings/admin.py
+++ server/apps/system_settings/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models.system_settings import SystemSettings
 
-admin.site.register(SystemSettings)
+
+class SystemSettingsAdmin(admin.ModelAdmin):
+    list_display = ('webcalendar_username', 'webcalendar_password', 'is_webcalendar_backup_active', 'variant')
+
+admin.site.register(SystemSettings, SystemSettingsAdmin)
