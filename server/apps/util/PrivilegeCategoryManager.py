3f7e72449b7e59e6f4dfd4b39e1f9f9f8dcd5282
Garrison Blair <garrisonblair96@gmail.com>
2018-11-21 16:57:23 -0500

    #20 assign privileges when users verify their accounts by calling the PrivilegeCategoryManager

--- server/apps/util/PrivilegeCategoryManager.py
+++ server/apps/util/PrivilegeCategoryManager.py
@@ -20,12 +20,17 @@ class PrivilegeCategoryManager:
         privilege_categories = PrivilegeCategory.objects.all()
         courses = ldap_server.get_user_groups(username=username)
 
+        # clear booker privileges before re-assigning them
         self.clear_booker_privileges(username)
 
+        # adding default privilege category that all users should be part of
         default_category = privilege_categories.get(is_default=True)
         if default_category:
             booker.privilege_categories.add(default_category)
 
+        # for each existing privilege category
+        # if there is no related course assigned to it, skip it
+        # if there is, check if the booker is registered for that course, and assign those privileges if so
         for category in privilege_categories:
             if category.related_course is None or category.related_course == "":
                 continue
