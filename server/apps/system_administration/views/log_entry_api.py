388db7ace33ffc34c6fe36ea494c5834cc3b6abc
Frederic Daigle <Freddydaigle@gmail.com>
2018-11-21 15:16:48 -0500

    #26 added filtering by from, to, content_type_id, object_id and user_id to Logentries

--- server/apps/system_administration/views/log_entry_api.py
+++ server/apps/system_administration/views/log_entry_api.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+from dateutil import parser as date_parser
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.admin.models import LogEntry
+from django.db.models.query import QuerySet
 
 from ..serializers.log_entry_serializer import LogEntrySerializer
 
@@ -8,7 +11,33 @@ from ..serializers.log_entry_serializer import LogEntrySerializer
 class LogEntryView(APIView):
 
     def get(self, request):
-        log_entries = LogEntry.objects.all()
+
+        # Get query params
+        start_datetime = request.GET.get("from")  # Datetime string
+        end_datetime = request.GET.get("to")  # Datetime string
+
+        model_id = request.GET.get("content_type_id")  # model content type id
+        object_id = request.GET.get("object_id")  # object id
+        user_id = request.GET.get("user_id")  # user id
+
+        log_entries = LogEntry.objects.all()  # type: QuerySet
+
+        if start_datetime is not None:
+            start_datetime = date_parser.parse(start_datetime, dayfirst=True)
+            log_entries = log_entries.filter(action_time__gte=start_datetime)
+
+        if end_datetime is not None:
+            end_datetime = date_parser.parse(end_datetime, dayfirst=True)
+            log_entries = log_entries.filter(action_time_lte=end_datetime)
+
+        if model_id is not None:
+            log_entries = log_entries.filter(content_type=model_id)
+
+        if object_id is not None:
+            log_entries = log_entries.filter(object_id=object_id)
+
+        if user_id is not None:
+            log_entries = log_entries.filter(user=user_id)
 
         serializer = LogEntrySerializer(log_entries, many=True)
 
