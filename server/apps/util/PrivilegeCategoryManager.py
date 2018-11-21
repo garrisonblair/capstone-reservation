74e0a3522d1a2e55913a202eb3d5d507e0988af1
Garrison Blair <garrisonblair96@gmail.com>
2018-11-21 15:58:35 -0500

    #20 change privilege category manager to use username

--- server/apps/util/PrivilegeCategoryManager.py
+++ server/apps/util/PrivilegeCategoryManager.py
@@ -5,22 +5,22 @@ from apps.accounts.models import Booker
 
 class PrivilegeCategoryManager:
 
-    def clear_booker_privileges(self, booker_id):
-        booker = Booker.objects.get(booker_id=booker_id)
+    def clear_booker_privileges(self, username):
+        booker = Booker.objects.get(user__username=username)
         booker.privilege_categories.clear()
         booker.save()
 
     def clear_all_booker_privileges(self):
         bookers = Booker.objects.all()
         for booker in bookers:
-            self.clear_booker_privileges(booker.booker_id)
+            self.clear_booker_privileges(booker.user.username)
 
-    def assign_booker_privileges(self, booker_id):
-        booker = Booker.objects.get(booker_id=booker_id)
+    def assign_booker_privileges(self, username):
+        booker = Booker.objects.get(user__username=username)
         privilege_categories = PrivilegeCategory.objects.all()
-        courses = ldap_server.get_user_groups(username=booker.user.username)
+        courses = ldap_server.get_user_groups(username=username)
 
-        self.clear_booker_privileges(booker_id)
+        self.clear_booker_privileges(username)
 
         default_category = privilege_categories.get(is_default=True)
         if default_category:
