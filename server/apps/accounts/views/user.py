333fc5bed48105cc8594c7ecd1ccc4263bffd540
Garrison Blair <garrisonblair96@gmail.com>
2018-11-21 19:57:02 -0500

    #20 fixed error with calling assign_booker_privileges

--- server/apps/accounts/views/user.py
+++ server/apps/accounts/views/user.py
@@ -67,7 +67,7 @@ class UserUpdate(APIView):
                 booker.save()
                 # Add Booker Privileges
                 manager = PrivilegeCategoryManager()
-                manager.assign_booker_privileges(user.booker)
+                manager.assign_booker_privileges(user.booker, None)
 
         # Must be superuser to update those fields
         if username and request.user.is_superuser:
