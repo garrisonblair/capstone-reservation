69bebb7ba906371e559245d209d18456d3674f8c
kemoua <kemoua@gmail.com>
2018-11-25 18:30:34 -0500

    Fixed Lint for Cells

--- client/src/components/Calendar/Cells.js
+++ client/src/components/Calendar/Cells.js
@@ -6,6 +6,15 @@ import BookingInfoModal from '../BookingInfoModal';
 
 
 class Cells extends Component {
+  static timeStringToInt(time) {
+    const tokens = time.split(':');
+    const timeInt = {
+      hour: parseInt(tokens[0], 10),
+      minutes: parseInt(tokens[1], 10),
+    };
+    return timeInt;
+  }
+
   state = {
     selectedHour: '',
     selectedRoomName: '',
@@ -15,6 +24,19 @@ class Cells extends Component {
     bookingModal: false,
     bookingInfoModal: false,
   };
+
+  /*
+   *COMPONENT LIFE CYCLE
+   */
+
+  static getDerivedStateFromProps(props, state) {
+    if (props.bookings === state.bookings) {
+      return null;
+    }
+    return {
+      bookings: props.bookings,
+    };
+  }
 
   /*
    * STYLE METHODS
@@ -72,6 +94,32 @@ class Cells extends Component {
   }
 
   /*
+  * HELPER METHOD
+  */
+
+  toggleBookingModal = () => {
+    const { bookingModal } = this.state;
+    this.setState({ bookingModal: !bookingModal });
+  }
+
+  toggleBookingInfoModal = () => {
+    const { bookingInfoModal } = this.state;
+    this.setState({ bookingInfoModal: !bookingInfoModal });
+  }
+
+  toggleBookingModalWithReservation = () => {
+    const { onCloseModalWithAction } = this.props;
+    this.setState({ bookingModal: false, bookingInfoModal: false });
+    onCloseModalWithAction();
+  }
+
+  toggleBookingInfoWithAction = () => {
+    const { onCloseModalWithAction } = this.props;
+    this.setState({ bookingModal: false, bookingInfoModal: false });
+    onCloseModalWithAction();
+  }
+
+  /*
    * CLICK HANDLING METHODS
    */
 
@@ -111,93 +159,39 @@ class Cells extends Component {
   }
 
   /*
-  * HELPER METHOD
+  * COMPONENT RENDERING
   */
 
-  toggleBookingModal = () => {
-    const { bookingModal } = this.state;
-    this.setState({ bookingModal: !bookingModal });
-  }
-
-  toggleBookingInfoModal = () => {
-    const { bookingInfoModal } = this.state;
-    this.setState({ bookingInfoModal: !bookingInfoModal });
-  }
-
-  toggleBookingModalWithReservation = () => {
-    const { onCloseModalWithAction } = this.props;
-    this.setState({ bookingModal: false, bookingInfoModal: false });
-    onCloseModalWithAction();
-  }
-
-  toggleBookingInfoWithAction = () => {
-    const { onCloseModalWithAction } = this.props;
-    this.setState({ bookingModal: false, bookingInfoModal: false });
-    onCloseModalWithAction();
-  }
-
-  timeStringToInt(time) {
-    let tokens = time.split(':');
-    let timeInt = {
-      hour: parseInt(tokens[0]),
-      minutes: parseInt(tokens[1]),
-    }
-    return timeInt;
-  }
-
-
-  getCamponsForBooking(booking) {
-    let campOns = []
-    if(!!this.props.campOns) {
-      this.props.campOns.map((campOn) => {
-        if(campOn.camped_on_booking == booking.id) {
-          campOns.push(campOn)
-        }
-      })
-    }
-    return campOns
-  }
-
-  /************* COMPONENT LIFE CYCLE *************/
-
-  static getDerivedStateFromProps(props, state) {
-    if(props.bookings === state.bookings) {
-      return null;
-    }
-    return {
-      bookings: props.bookings,
-    };
-  }
-
-  /************ COMPONENT RENDERING *************/
-
   renderCells() {
-    const {roomsList, hoursList} = this.props;
+    const { roomsList, hoursList, bookings } = this.props;
 
-    let cells = [];
+    const cells = [];
     let roomsCells = [];
     let cell = 0;
 
-    for (let i = 0; i < roomsList.length; i++) {
-      let currentRoom = roomsList[i];
-      for (let j = 0; j < hoursList.length; j++) {
-        let currentHour = hoursList[j];
+    for (let i = 0; i < roomsList.length; i += 1) {
+      const currentRoom = roomsList[i];
+      for (let j = 0; j < hoursList.length; j += 1) {
+        const currentHour = hoursList[j];
         roomsCells.push(
-          <div className="calendar__cells__cell" style={this.setCellStyle(j).cell_style} key={cell} onClick={() => this.handleClickCell(currentHour, currentRoom)}></div>
+          <div className="calendar__cells__cell" style={this.setCellStyle(j).cell_style} role="button" tabIndex="0" key={cell} onClick={() => this.handleClickCell(currentHour, currentRoom)} onKeyDown={() => {}} />,
         );
-        cell++;
+        cell += 1;
       }
 
-      let bookedCells = [];
-      if (this.props.bookings) {
-        let bookings = this.props.bookings.filter(booking => booking.room == currentRoom.id);
-        if (bookings.length > 0) {
-        bookedCells.push(this.renderCurrentBookings(bookings))
+      const bookedCells = [];
+      if (bookings) {
+        const bookingsList = bookings.filter(booking => booking.room === currentRoom.id);
+        if (bookingsList.length > 0) {
+          bookedCells.push(this.renderCurrentBookings(bookingsList));
         }
       }
 
       cells.push(
-        <div className="calendar__rooms__cells" key={i}>{roomsCells}{bookedCells}</div>
+        <div className="calendar__rooms__cells" key={i}>
+          {roomsCells}
+          {bookedCells}
+        </div>,
       );
 
       roomsCells = [];
@@ -206,82 +200,110 @@ class Cells extends Component {
   }
 
   renderCurrentBookings(bookings) {
-    let bookingsDiv = [];
+    const bookingsDiv = [];
 
-    bookings.forEach(booking => {
-      let campOns = this.getCamponsForBooking(booking);
+    bookings.forEach((booking) => {
+      const campOns = this.getCamponsForBooking(booking);
       bookingsDiv.push(
-        <div className="calendar__booking" style={this.setBookingStyle(booking, campOns.length).booking_style} key={booking.id} onClick={() => this.handleClickBooking(booking)}>
-          {booking.start_time.length > 5 ? booking.start_time.substring(0, booking.start_time.length-3): booking.start_time} - {booking.end_time.length > 5 ? booking.end_time.substring(0, booking.end_time.length-3): booking.end_time}
-          <br/>
+        <div className="calendar__booking" style={this.setBookingStyle(booking, campOns.length).booking_style} role="button" tabIndex="0" key={booking.id} onClick={() => this.handleClickBooking(booking)} onKeyDown={() => {}}>
+          {booking.start_time.length > 5
+            ? booking.start_time.substring(0, booking.start_time.length - 3) : booking.start_time}
+          -
+          {booking.end_time.length > 5
+            ? booking.end_time.substring(0, booking.end_time.length - 3) : booking.end_time}
+          <br />
           {(booking.end_time.replace(/:/g, '') - booking.start_time.replace(/:/g, '')) < 4000 ? null : <span>{booking.booker.user.username}</span>}
           {campOns.length > 0 ? this.renderCampOns(campOns) : ''}
-        </div>
-      )
+        </div>,
+      );
     });
     return bookingsDiv;
   }
 
-  renderCampOns(campOns) {
-    let text = [<span key='-1'>[CAMP]<br/></span>]
-    campOns.forEach(campOn => {
+  static renderCampOns(campOns) {
+    const text = [
+      <span key="-1">
+        [CAMP]
+        <br />
+      </span>];
+    campOns.forEach((campOn) => {
       text.push(
-        <span key={campOn.id}>{campOn.start_time.substring(0, campOn.start_time.length-3)} - {campOn.end_time.substring(0, campOn.end_time.length-3)}<br/></span>
-      )
-    })
-    return <span><br/>{text}</span>
+        <span key={campOn.id}>
+          {campOn.start_time.substring(0, campOn.start_time.length - 3)}
+          -
+          {campOn.end_time.substring(0, campOn.end_time.length - 3)}
+          <br />
+        </span>,
+      );
+    });
+    return (
+      <span>
+        <br />
+        {text}
+      </span>
+    );
   }
 
   render() {
+    const {
+      bookingModal,
+      selectedRoomId,
+      selectedRoomName,
+      selectedHour,
+      selectedRoomCurrentBookings,
+      selectedBookingId,
+      bookingInfoModal,
+      selectedBooking,
+    } = this.state;
+    const { selectedDate } = this.props;
     return (
       <div>
         {this.renderCells()}
 
         <ReservationDetailsModal
-          show={this.state.bookingModal}
-          selectedRoomId={this.state.selectedRoomId}
-          selectedRoomName={this.state.selectedRoomName}
-          selectedHour={this.state.selectedHour}
-          selectedDate={this.props.selectedDate}
-          selectedRoomCurrentBookings={this.state.selectedRoomCurrentBookings}
-          selectedBookingId={this.state.selectedBookingId}
+          show={bookingModal}
+          selectedRoomId={selectedRoomId}
+          selectedRoomName={selectedRoomName}
+          selectedHour={selectedHour}
+          selectedDate={selectedDate}
+          selectedRoomCurrentBookings={selectedRoomCurrentBookings}
+          selectedBookingId={selectedBookingId}
           onClose={this.toggleBookingModal}
           onCloseWithReservation={this.toggleBookingModalWithReservation}
         />
 
         <BookingInfoModal
-          show={this.state.bookingInfoModal}
-          booking={this.state.selectedBooking}
-          selectedRoomName={this.state.selectedRoomName}
+          show={bookingInfoModal}
+          booking={selectedBooking}
+          selectedRoomName={selectedRoomName}
           onClose={this.toggleBookingInfoModal}
           onCloseWithAction={this.toggleBookingInfoWithAction}
         />
       </div>
-    )
+    );
   }
 }
 
 Cells.propTypes = {
-  roomsList: PropTypes.array,
-  hoursList: PropTypes.array,
-  hoursSettings: PropTypes.object,
-  bookings: PropTypes.array,
-  campOns: PropTypes.array,
-  selectedDate: PropTypes.object,
+  roomsList: PropTypes.instanceOf(Array),
+  hoursList: PropTypes.instanceOf(Array),
+  hoursSettings: PropTypes.instanceOf(Object),
+  bookings: PropTypes.instanceOf(Array),
+  selectedDate: PropTypes.instanceOf(Object),
   onCloseModalWithAction: PropTypes.func,
-}
+};
 
 Cells.defaultProps = {
   roomsList: [],
   hoursList: [],
   hoursSettings: {
-                    start: "08:00",
-                    end: "23:00",
-                    increment: 60
-                  },
+    start: '08:00',
+    end: '23:00',
+    increment: 60,
+  },
   bookings: [],
-  campOns: [],
   selectedDate: new Date(),
-}
+  onCloseModalWithAction: () => {},
+};
 
 export default Cells;
