91885a4ae57d6ca7e813df894e12574e83a0f5ad
Frederic Daigle <Freddydaigle@gmail.com>
2018-12-03 15:49:59 -0500

    #26 Added Value Help view for Content types.  Needed by UI for filtering

---
+++ server/apps/system_administration/views/content_type_value_help.py
@@ -0,0 +1,20 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.contrib.contenttypes.models import ContentType
+from ..serializers.log_entry_serializer import ContentTypeSerializer
+
+
+class ContentTypeValueHelp(APIView):
+
+    exposed_models = [
+        "booking",
+        "campon",
+        "recurringbooking"
+    ]
+
+    def get(self, request):
+        content_types = ContentType.objects.filter(model__in=ContentTypeValueHelp.exposed_models)
+        serializer = ContentTypeSerializer(content_types, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
