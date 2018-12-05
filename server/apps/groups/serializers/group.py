a35983cc7bada49e48af23e40851eab8b7e8723c
steve locke <tap3stry@gmail.com>
2018-12-05 10:44:01 -0500

    #15 #16 Updated serializer

--- server/apps/groups/serializers/group.py
+++ server/apps/groups/serializers/group.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from ..models.Group import Group
 
+from apps.accounts.serializers.user import BookerSerializer
+
 
 class GroupSerializer(serializers.ModelSerializer):
+
+    owner = BookerSerializer()
+    members = BookerSerializer(many=True)
+
     class Meta:
         model = Group
         fields = '__all__'
