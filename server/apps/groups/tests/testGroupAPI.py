52676e6875f0c8537427a1eff8435b3aeab5bf28
steve locke <tap3stry@gmail.com>
2018-12-05 11:25:21 -0500

    #15 #16 Updated serializer

--- server/apps/groups/tests/testGroupAPI.py
+++ server/apps/groups/tests/testGroupAPI.py
@@ -71,10 +71,16 @@ class RoomAPITest(TestCase):
     def testCreateGroups(self):
         request = self.factory.post("/group",
                                     {
-                                        "name": "The Group Name"
+                                        "name": "The Group Name",
+                                        "owner": self.user.booker
                                     }, format="json")
+
         force_authenticate(request, user=self.user)
+
         response = GroupCreate.as_view()(request)
+
+        print(response)
+
         group = Group.objects.get(name="The Group Name", owner=self.user.booker)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -87,23 +93,27 @@ class RoomAPITest(TestCase):
                                         "members": ["booker_2"]
                                     }, format="json")
         force_authenticate(request, user=self.user)
-        response = AddMembers.as_view()(request, 1)
-        group = Group.objects.get(id=1)
-        self.assertEqual(len(group.members.all()), 2)
 
+        response = AddMembers.as_view()(request, 1)
+
+        group = Group.objects.get(id=1)
+
+        self.assertEqual(len(group.members.all()), 2)
         self.assertTrue(self.booker_2 in group.members.all())
 
     def testRemoveMember(self):
         self.group1.members.add(self.booker_2)
-
         self.group1.save()
+
         request = self.factory.post("group/" + str(self.group1.id) + "/remove_members",
                                     {
                                     "members": ["booker_2"]
                                     }, format="json")
+
         force_authenticate(request, user=self.user)
 
         response = RemoveMembers.as_view()(request, self.group1.id)
+
         self.group1.refresh_from_db()
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
         self.assertEqual(len(self.group1.members.all()), 1)
