130c545aa3a3a80f810a4fcb641db601fb6d9e88
Garrison Blair <garrisonblair96@gmail.com>
2018-12-19 18:08:55 +0100

    #104 Fixed PrivilegeRequest model fields as well as save method

--- server/apps/groups/models/PrivilegeRequest.py
+++ server/apps/groups/models/PrivilegeRequest.py
@@ -6,21 +6,25 @@ from apps.accounts.models.PrivilegeCategory import PrivilegeCategory
 
 
 class PrivilegeRequest(models.Model):
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
-    privilege_category = models.ForeignKey(PrivilegeCategory, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, null=True, on_delete=models.CASCADE)
+    privilege_category = models.ForeignKey(PrivilegeCategory, null=True, on_delete=models.CASCADE)
     submission_date = models.DateField()
+
+    PE = 'Pending'
+    DE = 'Denied'
+    AP = 'Approved'
     STATUS_CHOICES = (
-        ('PE', "Pending"),
-        ('DE', "Denied"),
-        ('AP', "Approved")
+        (PE, 'Pending'),
+        (DE, 'Denied'),
+        (AP, 'Approved')
     )
     status = models.CharField(
-        max_length=2,
+        max_length=8,
         choices=STATUS_CHOICES,
-        default='PE'
+        default=PE
     )
 
     def save(self, *args, **kwargs):
         if not self.id:
             self.submission_date = datetime.datetime.now().date()
-            return super(PrivilegeRequest, self).save(*args, **kwargs)
+        return super(PrivilegeRequest, self).save(*args, **kwargs)
