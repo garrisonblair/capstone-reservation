10e8d12111fb281ad2d0fa896caa2893078376a1
Garrison Blair <garrisonblair96@gmail.com>
2018-12-21 00:28:39 +0100

    #104 merge the endpoints for listing requests with logic to display all when admin, and only users' when regular bookers

--- server/apps/groups/urls.py
+++ server/apps/groups/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views.groups import GroupList, AddMembers, GroupCreate, RemoveMembers
 from .views.group_privileges import PrivilegeRequestList, PrivilegeRequestCreate
-from .views.group_privileges import PrivilegeRequestsList, ApprovePrivilegeRequest, DenyPrivilegeRequest
+from .views.group_privileges import ApprovePrivilegeRequest, DenyPrivilegeRequest
 
 
 urlpatterns = [
@@ -9,11 +9,9 @@ urlpatterns = [
     path(r'group/<int:pk>/remove_members', RemoveMembers.as_view()),
     path(r'group', GroupCreate.as_view()),
     path(r'groups', GroupList.as_view()),
-    path(r'my_privilege_requests', PrivilegeRequestList.as_view()),
-    path(r'my_privilege_requests/<str:status>', PrivilegeRequestList.as_view()),
     path(r'request_privilege', PrivilegeRequestCreate.as_view()),
-    path(r'privilege_requests', PrivilegeRequestsList.as_view()),
-    path(r'privilege_requests/<str:status>', PrivilegeRequestsList.as_view()),
+    path(r'privilege_requests', PrivilegeRequestList.as_view()),
+    path(r'privilege_requests/<str:status>', PrivilegeRequestList.as_view()),
     path(r'approve_privilege_request', ApprovePrivilegeRequest.as_view()),
     path(r'deny_privilege_request', DenyPrivilegeRequest.as_view())
 ]
--- server/apps/groups/views/group_privileges.py
+++ server/apps/groups/views/group_privileges.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.core.mail import send_mail
 from rest_framework.generics import ListAPIView
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from apps.accounts.permissions.IsSuperUser import IsSuperUser
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -21,20 +21,18 @@ class PrivilegeRequestList(ListAPIView):
 
     def get_queryset(self):
         qs = super(PrivilegeRequestList, self).get_queryset()
-        try:
-            owner = Booker.objects.get(booker_id=self.request.user.booker)
-            owned_groups = owner.owned_groups.all()
-            qs = qs.filter(group__in=owned_groups)
-        except Booker.DoesNotExist:
-            pass
+
+        if not self.request.user.is_superuser:
+            try:
+                owner = Booker.objects.get(booker_id=self.request.user.booker)
+                owned_groups = owner.owned_groups.all()
+                qs = qs.filter(group__in=owned_groups)
+            except Booker.DoesNotExist:
+                pass
 
         request_status = self.request.GET.get('status')
-        if request_status == PrivilegeRequest.PE:
-            qs = qs.filter(status=PrivilegeRequest.PE)
-        elif request_status == PrivilegeRequest.DE:
-            qs = qs.filter(status=PrivilegeRequest.DE)
-        elif request_status == PrivilegeRequest.AP:
-            qs = qs.filter(status=PrivilegeRequest.AP)
+        if request_status is not None:
+            qs = qs.filter(status=request_status)
 
         return qs
 
@@ -63,25 +61,6 @@ class PrivilegeRequestCreate(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         except ValidationError as error:
             return Response(error.messages, status=status.HTTP_400_BAD_REQUEST)
-
-
-class PrivilegeRequestsList(ListAPIView):
-    permission_classes = (IsAuthenticated, IsSuperUser)
-    serializer_class = ReadPrivilegeRequestSerializer
-    queryset = PrivilegeRequest.objects.all()
-
-    def get_queryset(self):
-        qs = super(PrivilegeRequestsList, self).get_queryset()
-
-        request_status = self.request.GET.get('status')
-        if request_status == PrivilegeRequest.PE:
-            qs = qs.filter(status=PrivilegeRequest.PE)
-        elif request_status == PrivilegeRequest.DE:
-            qs = qs.filter(status=PrivilegeRequest.DE)
-        elif request_status == PrivilegeRequest.AP:
-            qs = qs.filter(status=PrivilegeRequest.AP)
-
-        return qs
 
 
 class ApprovePrivilegeRequest(APIView):
