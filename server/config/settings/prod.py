e2da1eefa0e613179e506e8e3d45334e4ee0770f
xTEddie <edwardtranv1@gmail.com>
2018-12-21 14:19:37 -0500

    Use MySQL by default in prod

--- server/config/settings/prod.py
+++ server/config/settings/prod.py
@@ -5,7 +5,9 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
     }
 }
