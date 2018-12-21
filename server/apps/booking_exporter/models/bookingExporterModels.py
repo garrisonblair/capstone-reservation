bfa0fcdbaa224b12b259e94011d5e0b603f0e177
xTEddie <edwardtranv1@gmail.com>
2018-12-21 13:38:51 -0500

    Fix models for mysql

--- server/apps/booking_exporter/models/bookingExporterModels.py
+++ server/apps/booking_exporter/models/bookingExporterModels.py
@@ -5,4 +5,4 @@ from apps.rooms.models.Room import Room
 
 class ExternalRoomID(models.Model):
     room = models.OneToOneField(Room, on_delete=models.CASCADE)
-    external_id = models.TextField(unique=True)
+    external_id = models.TextField()
