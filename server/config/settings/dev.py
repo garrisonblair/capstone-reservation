432c1b7ed09f69c4c5ccdad8533621df03f25d0c
xTEddie <edwardtranv1@gmail.com>
2018-12-21 13:20:08 -0500

    Change django settings to use mysql

--- server/config/settings/dev.py
+++ server/config/settings/dev.py
@@ -5,9 +5,18 @@ from .email import *
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
     }
 }
