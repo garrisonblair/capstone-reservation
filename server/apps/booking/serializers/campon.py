43e3ae90af962fb0c299fc0fd90ffa319f5059fc
Garrison Blair <garrisonblair96@gmail.com>
2018-12-23 11:25:25 +0100

    #103 #122 Changed Camp-on serializer to return username of the booker's user entity as well

--- server/apps/booking/serializers/campon.py
+++ server/apps/booking/serializers/campon.py
@@ -9,8 +9,12 @@ class CampOnSerializer(serializers.ModelSerializer):
     booker = serializers.PrimaryKeyRelatedField(queryset=Booker.objects.all())
     camped_on_booking = serializers.PrimaryKeyRelatedField(queryset=Booking.objects.all())
     generated_booking = serializers.PrimaryKeyRelatedField(read_only=True)
+    username = serializers.SerializerMethodField()
 
     class Meta:
         model = CampOn
-        fields = ('id', 'booker', 'camped_on_booking', 'generated_booking', 'start_time', 'end_time')
+        fields = ('id', 'booker', 'camped_on_booking', 'generated_booking', 'start_time', 'end_time', 'username')
         read_only_fields = ('id',)
+
+    def get_username(self, camp_on):
+        return camp_on.booker.user.username
