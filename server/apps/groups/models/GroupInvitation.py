6e4e969f8c8fb592dacf13a9008d1537d57d6cac
Frederic Daigle <Freddydaigle@gmail.com>
2018-12-27 13:53:01 -0500

    #95 created Group Invitation model

---
+++ server/apps/groups/models/GroupInvitation.py
@@ -0,0 +1,10 @@
+from django.db import models
+from apps.accounts.models.Booker import Booker
+from .Group import Group
+
+
+class GroupInvitation(models.Model):
+
+    invited_booker = models.ForeignKey(Booker, related_name="group_invitations")
+    group = models.ForeignKey(Group, related_name="invitations")
+    timestamp = models.DateTimeField()
