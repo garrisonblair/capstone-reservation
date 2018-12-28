8f61f0f917e13e7830bf6ee6023a7f82ca9fc448
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2018-12-28 11:01:29 -0500

    #101 create Bookers tab in admin page

---
+++ client/src/components/Admin/Bookers/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Bookers.scss';
+
+
+class Bookers extends Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div id="bookers">
+        <h2>Bookers</h2>
+      </div>
+    );
+  }
+}
+
+export default Bookers;
--- client/src/index.js
+++ client/src/index.js
@@ -12,6 +12,7 @@ import RoomManager from './components/Admin/RoomManager';
 // import Stats from './components/Admin/Stats';
 import BookingActivity from './components/Admin/Stats/BookingActivity';
 import Settings from './components/Admin/Settings';
+import Bookers from './components/Admin/Bookers';
 
 
 ReactDOM.render(
@@ -27,6 +28,7 @@ ReactDOM.render(
         <Route exact path="/admin/privileges" render={() => <Admin menuType="privileges" content={<PrivilegeCategory />} />} />
         <Route exact path="/admin/rooms" render={() => <Admin menuType="rooms" content={<RoomManager />} />} />
         <Route exact path="/admin/stats" render={() => <Admin menuType="stats" content={<BookingActivity />} />} />
+        <Route exact path="/admin/bookers" render={() => <Admin menuType="bookers" content={<Bookers />} />} />
         <Route component={NotFound} />
       </Switch>
     </div>
