f432a512daf1913f916194d691e8c0325e8e9154
Garrison Blair <garrisonblair96@gmail.com>
2019-01-03 18:44:39 +0100

    #133 Created view for returning all bookers, displaying info from user and privilege category serializers. allows to filter using the username or first or last name of booker's user

--- server/apps/accounts/urls.py
+++ server/apps/accounts/urls.py
@@ -3,7 +3,7 @@ from apps.accounts.views.register import RegisterView
 from apps.accounts.views.verify import VerifyView
 from apps.accounts.views.login import LoginView
 from apps.accounts.views.logout import LogoutView
-from apps.accounts.views.user import UserList, UserUpdate
+from apps.accounts.views.user import UserList, UserUpdate, BookerList
 from apps.accounts.views.me import MyUser
 from apps.accounts.views.privilege_categories import PrivilegeCategoryList
 from apps.accounts.views.privilege_categories import MyPrivilegeCategoryList
@@ -21,6 +21,7 @@ urlpatterns = [
 
     path(r'users', UserList.as_view()),
     path(r'user/<int:pk>', UserUpdate.as_view()),
+    path(r'bookers', BookerList.as_view()),
     path(r'me', MyUser.as_view()),
 
     path(r'privilege_categories', PrivilegeCategoryList.as_view()),
