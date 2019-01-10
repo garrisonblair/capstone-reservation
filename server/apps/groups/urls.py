07aeb899190679c5cf9385dd12253a5a7a4b07b3
steve locke <tap3stry@gmail.com>
2019-01-09 23:34:53 -0500

    #16 Added url for group API endpoint for leaving group

--- server/apps/groups/urls.py
+++ server/apps/groups/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views.groups import GroupList, AddMembers, GroupCreate, RemoveMembers
+from .views.groups import GroupList, AddMembers, GroupCreate, RemoveMembers, LeaveGroup
 from .views.group_privileges import PrivilegeRequestList, PrivilegeRequestCreate
 from .views.group_privileges import ApprovePrivilegeRequest, DenyPrivilegeRequest
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path(r'group/<int:pk>/remove_members', RemoveMembers.as_view()),
     path(r'group', GroupCreate.as_view()),
     path(r'groups', GroupList.as_view()),
+    path(r'group/<int:pk>/leave_group', LeaveGroup.as_view()),
     path(r'request_privilege', PrivilegeRequestCreate.as_view()),
     path(r'privilege_requests', PrivilegeRequestList.as_view()),
     path(r'privilege_requests/<str:status>', PrivilegeRequestList.as_view()),
