84099cc35d1139b4ad7a17069764c8c0ca32ec66
Frederic Daigle <Freddydaigle@gmail.com>
2019-01-11 20:37:22 -0500

    #109 Fixes room, system admin and util tests, except one last one

--- server/apps/util/PrivilegeCategoryManager.py
+++ server/apps/util/PrivilegeCategoryManager.py
@@ -1,22 +1,22 @@
 from apps.util import ldap_server
 from apps.accounts.models.PrivilegeCategory import PrivilegeCategory
-from apps.accounts.models import BookerProfile
+from apps.accounts.models.User import User
 
 
 class PrivilegeCategoryManager:
 
     def clear_booker_privileges(self, booker):
-        booker.privilege_categories.clear()
+        booker.bookerprofile.privilege_categories.clear()
         booker.save()
 
     def clear_all_booker_privileges(self):
-        bookers = BookerProfile.objects.all()
+        bookers = User.objects.all()
         for booker in bookers:
             self.clear_booker_privileges(booker)
 
     def assign_booker_privileges(self, booker, server=ldap_server):
         privilege_categories = PrivilegeCategory.objects.all()
-        courses = server.get_user_groups(username=booker.user.username)
+        courses = server.get_user_groups(username=booker.username)
 
         # clear booker privileges before re-assigning them
         self.clear_booker_privileges(booker)
@@ -24,7 +24,7 @@ class PrivilegeCategoryManager:
         # adding default privilege category that all users should be part of
         default_category = privilege_categories.get(is_default=True)
         if default_category:
-            booker.privilege_categories.add(default_category)
+            booker.bookerprofile.privilege_categories.add(default_category)
 
         # if user is not found in the LDAP he will only have default privileges
         if courses is None:
@@ -39,11 +39,11 @@ class PrivilegeCategoryManager:
             for course in courses:
                 course.lower()
                 if category.related_course in course:
-                    booker.privilege_categories.add(category)
+                    booker.bookerprofile.privilege_categories.add(category)
 
         booker.save()
 
     def assign_all_booker_privileges(self, server=ldap_server):
-        bookers = BookerProfile.objects.all()
+        bookers = User.objects.all()
         for booker in bookers:
             self.assign_booker_privileges(booker, server)
