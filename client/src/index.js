bd398635fb49aa5df508906e4ffaac86a41de43c
xTEddie <edwardtranv1@gmail.com>
2019-01-16 12:45:56 -0500

    Disable login route since it's a modal now

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
-import Login from './components/Login';
+// import Login from './components/Login';
 import NotFound from './components/NotFound';
 import Registration from './components/Registration';
 import Verification from './components/Verification';
@@ -21,7 +21,7 @@ ReactDOM.render(
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
+        {/* <Route exact path="/login" component={Login} /> */}
         <Route exact path="/registration" component={Registration} />
         <Route exact path="/verify/:token" component={Verification} />
         <Route exact path="/admin" render={() => <Admin menuType="settings" content={<Settings />} />} />
