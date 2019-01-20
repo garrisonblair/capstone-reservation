d2ec3015e09df57cd43f8ba416d43c2ab1e63596
steve locke <tap3stry@gmail.com>
2019-01-20 13:17:05 -0500

    #13 Changed the end time of subsequent campons to be based on the end time of pervious campon instead of the start of the difference booking as there may not always be a difference booking

--- server/apps/booking/views/booking.py
+++ server/apps/booking/views/booking.py
@@ -137,7 +137,8 @@ class BookingCancel(APIView):
                             start_time=previous_campon.end_time,
                             end_time=campon.end_time)
                         difference_booking.save()
-                        campon.end_time = difference_booking.start_time
+                        #campon.end_time = difference_booking.start_time
+                    campon.end_time = previous_campon.end_time
                     campon.save()
 
                 previous_campon = campon
