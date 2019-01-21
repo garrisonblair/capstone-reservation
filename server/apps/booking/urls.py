147e889a43cb3a47dde1444fedf955369b235f64
steve locke <tap3stry@gmail.com>
2019-01-21 18:57:49 -0500

    #11 Added url for view my bookings

--- server/apps/booking/urls.py
+++ server/apps/booking/urls.py
@@ -1,19 +1,17 @@
 from django.urls import path
 
 from .views.booking import BookingList
-from .views.booking import BookingCreate
+from .views.booking import BookingCreate, BookingRetrieveUpdateDestroy, BookingViewMyBookings
 from .views.campon import CampOnList
 from .views.campon import CampOnCreate
-from .views.booking import BookingRetrieveUpdateDestroy
 from .views.recurring_booking import RecurringBookingCreate
 
 urlpatterns = [
     path(r'bookings', BookingList.as_view()),
     path(r'booking', BookingCreate.as_view()),
     path(r'booking/<int:pk>', BookingRetrieveUpdateDestroy.as_view()),
-
+    path(r'my_bookings/', BookingViewMyBookings.as_view()),
     path(r'campons', CampOnList.as_view()),
     path(r'campon', CampOnCreate.as_view()),
-
     path(r'recurring_booking', RecurringBookingCreate.as_view())
 ]
