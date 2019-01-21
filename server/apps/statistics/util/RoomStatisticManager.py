5342ba89965f72a7ea0b3e248b7c4dd86feb9d2b
Garrison Blair <garrisonblair96@gmail.com>
2019-01-21 18:10:27 -0500

    #25 fixed db issue

--- server/apps/statistics/util/RoomStatisticManager.py
+++ server/apps/statistics/util/RoomStatisticManager.py
@@ -6,6 +6,11 @@ from apps.rooms.serializers.room import RoomSerializer
 
 
 class RoomStatisticManager:
+    def get_first_booking_date(self):
+        if Booking.objects.count() == 0:
+            return datetime.datetime.now().date()
+        return Booking.objects.order_by("date").first().date
+
     def get_num_room_bookings(self, room, start_date=None, end_date=None):
         return room.get_bookings(start_date, end_date).count()
 
@@ -16,8 +21,11 @@ class RoomStatisticManager:
         return time_booked
 
     def get_average_bookings_per_day(self, room,
-                                     start_date=Booking.get_first_booking_date(),
+                                     start_date=None,
                                      end_date=datetime.datetime.now().date()):
+        if start_date is None:
+            start_date = self.get_first_booking_date()
+
         total_days = end_date - start_date
         total_days = total_days.days + 1
         decimal.getcontext().prec = 3
@@ -25,8 +33,11 @@ class RoomStatisticManager:
         return float(decimal.Decimal(num_room_bookings) / decimal.Decimal(total_days))
 
     def get_average_time_booked_per_day(self, room,
-                                        start_date=Booking.get_first_booking_date(),
+                                        start_date=None,
                                         end_date=datetime.datetime.now().date()):
+        if start_date is None:
+            start_date = self.get_first_booking_date()
+
         total_days = end_date - start_date
         total_days = total_days.days + 1
         decimal.getcontext().prec = 3
