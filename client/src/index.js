03b6d8d8451dc54126295ae67ca3ef9bd0f9e853
xTEddie <edwardtranv1@gmail.com>
2019-01-22 18:57:54 -0500

    #11 Remove unused routes since those components are displayed in /profile

--- client/src/index.js
+++ client/src/index.js
@@ -12,9 +12,7 @@ import RoomManager from './components/Admin/RoomManager';
 import Stats from './components/Admin/Stats';
 import BookingActivity from './components/Admin/Stats/BookingActivity';
 import Settings from './components/Admin/Settings';
-import Groups from './components/Groups';
 import Bookers from './components/Admin/Bookers';
-import Privileges from './components/Privileges';
 import Profile from './components/Profile';
 
 
@@ -30,12 +28,10 @@ ReactDOM.render(
         <Route exact path="/admin/settings" render={() => <Admin menuType="settings" content={<Settings />} />} />
         <Route exact path="/admin/privileges" render={() => <Admin menuType="privileges" content={<PrivilegeCategory />} />} />
         <Route exact path="/admin/rooms" render={() => <Admin menuType="rooms" content={<RoomManager />} />} />
-        <Route exact path="/myGroups" component={Groups} />
         <Route exact path="/admin/logs" render={() => <Admin menuType="logs" content={<BookingActivity />} />} />
         <Route exact path="/admin/stats" render={() => <Admin menuType="stats" content={<Stats />} />} />
         <Route exact path="/admin/bookers" render={() => <Admin menuType="bookers" content={<Bookers />} />} />
         <Route exact path="/profile" component={Profile} />
-        <Route exact path="/privileges" component={Privileges} />
         <Route component={NotFound} />
       </Switch>
     </div>
