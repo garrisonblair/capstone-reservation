cd24f810057a8ea27bb93ab9a6779680fb479ca2
xTEddie <edwardtranv1@gmail.com>
2019-01-22 20:03:26 -0500

    #11 Display bookings in the table

--- client/src/components/UserBookings/index.js
+++ client/src/components/UserBookings/index.js
@@ -1,23 +1,83 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
+import {
+  Table, TableBody,
+} from 'semantic-ui-react';
 import api from '../../utils/api';
 
 
-// eslint-disable-next-line react/prefer-stateless-function
 class UserBookings extends Component {
+  state = {
+    bookings: [],
+  }
+
   componentDidMount() {
     api.getBookings()
       .then((response) => {
-        const { data } = response;
-        // eslint-disable-next-line no-console
-        console.log(data);
+        const { data: bookings } = response;
+        this.setState({
+          bookings,
+        });
       });
+  }
+
+  renderTableHeader = () => {
+    const headers = ['Room', 'Date', 'Start', 'End', 'Group'];
+    let component = [];
+    component = headers.map((header, index) => (
+      // eslint-disable-next-line react/no-array-index-key
+      <Table.HeaderCell key={index}>
+        {header}
+      </Table.HeaderCell>
+    ));
+
+    return (
+      <Table.Header>
+        <Table.Row>
+          {component}
+        </Table.Row>
+      </Table.Header>
+    );
+  }
+
+  renderTableBody = () => {
+    const { bookings } = this.state;
+    let rows = [];
+    rows = bookings.map((booking, index) => (
+      // eslint-disable-next-line react/no-array-index-key
+      <Table.Row key={index}>
+        <Table.Cell>
+          {booking.room}
+        </Table.Cell>
+        <Table.Cell>
+          {booking.date}
+        </Table.Cell>
+        <Table.Cell>
+          {booking.start_time}
+        </Table.Cell>
+        <Table.Cell>
+          {booking.end_time}
+        </Table.Cell>
+        <Table.Cell>
+          {booking.group}
+        </Table.Cell>
+      </Table.Row>
+    ));
+    return (
+      <TableBody>
+        {rows}
+      </TableBody>
+    );
   }
 
   render() {
     return (
       <div>
         <h1> Bookings </h1>
+        <Table>
+          {this.renderTableHeader()}
+          {this.renderTableBody()}
+        </Table>
       </div>
     );
   }
