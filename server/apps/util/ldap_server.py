a69437e2bdfc3cd508b665c0efa0118a53672567
Frederic Daigle <Freddydaigle@gmail.com>
2019-01-30 15:36:34 -0500

    #162 need to override get_user because some users have duplicate accounts with suffixes.  Need to filter for the exact name match

--- server/apps/util/ldap_server.py
+++ server/apps/util/ldap_server.py
@@ -1,5 +1,6 @@
 import ldap3
 from django_python3_ldap import ldap
+from django_python3_ldap import utils
 from django_python3_ldap.conf import settings
 
 
@@ -29,13 +30,36 @@ def get_ldap_connection():
             user=settings.LDAP_AUTH_CONNECTION_USERNAME,
             password=settings.LDAP_AUTH_CONNECTION_PASSWORD,
             auto_bind=auto_bind,
-            raise_exceptions=False,
+            raise_exceptions=True,
             receive_timeout=settings.LDAP_AUTH_RECEIVE_TIMEOUT,
         )
         return ldap.Connection(connection)
     except Exception as ex:
         print("LDAP connect failed: {ex}".format(ex=ex))
 
+    return None
+
+
+def get_user(**kwargs):
+    """
+    Returns the user with the given identifier.
+
+    The user identifier should be keyword arguments matching the fields
+    in settings.LDAP_AUTH_USER_LOOKUP_FIELDS.
+    """
+    connection = get_ldap_connection()
+    if connection._connection.search(
+            search_base=settings.LDAP_AUTH_SEARCH_BASE,
+            search_filter=utils.format_search_filter(kwargs),
+            search_scope=ldap3.SUBTREE,
+            attributes=ldap3.ALL_ATTRIBUTES,
+            get_operational_attributes=True,
+            size_limit=5,
+    ):
+
+        for response in connection._connection.response:
+            if response["attributes"]["name"][0] == kwargs["username"]:
+                return connection._get_or_create_user(response)
     return None
 
 
