956449c580bcd9f48b4ebdcb33571132534413e6
Edward Tran <edwardtranv1@gmail.com>
2019-01-30 13:18:50 -0500

    Add script to populate prod DB

---
+++ server/scripts/populate_prod_db.sh
@@ -0,0 +1,4 @@
+python manage.py makemigrations
+python manage.py migrate
+python manage.py loaddata apps/accounts/fixtures/privilege_categories.json
+python manage.py loaddata apps/rooms/fixtures/rooms.json
