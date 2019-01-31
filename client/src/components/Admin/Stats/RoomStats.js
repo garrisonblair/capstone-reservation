85470fe089a4c0d1feb05376bd8206424db507fd
xTEddie <edwardtranv1@gmail.com>
2019-01-30 23:19:42 -0500

    #25 Add segment to filter start and end date

--- client/src/components/Admin/Stats/RoomStats.js
+++ client/src/components/Admin/Stats/RoomStats.js
@@ -1,11 +1,64 @@
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Dropdown } from 'semantic-ui-react';
+import {
+  Button,
+  Dropdown,
+  Form,
+  Icon,
+  Input,
+  Segment,
+} from 'semantic-ui-react';
+import { SingleDatePicker } from 'react-dates';
+// import * as moment from 'moment';
 import { Bar } from 'react-chartjs-2';
+import 'react-dates/lib/css/_datepicker.css';
+
 
 class RoomStats extends Component {
   state = {
     selected: ['Hours Booked'],
+    date: null,
+    focused: false,
+    focusStartDate: false,
+    focusEndDate: false,
+    startDate: '',
+    endDate: '',
+  }
+
+  handleChangeDate = (date) => {
+    const { focusStartDate, focusEndDate } = this.state;
+    let { startDate, endDate } = this.state;
+
+    if (focusStartDate === true) {
+      startDate = date.format('YYYY-MM-DD');
+    }
+
+    if (focusEndDate === true) {
+      endDate = date.format('YYYY-MM-DD');
+    }
+
+    this.setState({
+      // date,
+      startDate,
+      endDate,
+    });
+  }
+
+  handleDatePickerFocusChange = (f) => {
+    let focusStartDate = true;
+    let focusEndDate = true;
+    if (f === false) {
+      focusStartDate = false;
+      focusEndDate = false;
+    }
+    this.setState({
+      focusStartDate,
+      focusEndDate,
+      focused: f,
+    });
   }
 
   onDropDownChange = (event, data) => {
@@ -29,6 +82,64 @@ class RoomStats extends Component {
         options={stateOptions}
         onChange={this.onDropDownChange}
       />
+    );
+  }
+
+  renderDateFilter = () => {
+    const {
+      date,
+      focused,
+      focusStartDate,
+      focusEndDate,
+      startDate,
+      endDate,
+    } = this.state;
+
+    return (
+      <Segment>
+        <Form>
+          <Form.Group>
+            <Form.Field>
+              <label>from</label>
+              <Input
+                icon="calendar alternate"
+                iconPosition="left"
+                placeholder="Start Date"
+                focus={focusStartDate}
+                value={startDate}
+                onClick={() => this.setState({ focusStartDate: true, focused: true })}
+                onChange={e => this.setState({ startDate: e.target.value })}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>to</label>
+              <Input
+                icon="calendar alternate outline"
+                iconPosition="left"
+                placeholder="End Date"
+                focus={focusEndDate}
+                value={endDate}
+                onClick={() => this.setState({ focusEndDate: true, focused: true })}
+                onChange={e => this.setState({ endDate: e.target.value })}
+              />
+            </Form.Field>
+          </Form.Group>
+          <SingleDatePicker
+            isOutsideRange={() => false}
+            numberOfMonths={1}
+            date={date}
+            onDateChange={d => this.handleChangeDate(d)}
+            focused={focused}
+            onFocusChange={({ focused: f }) => this.handleDatePickerFocusChange(f)}
+            hideKeyboardShortcutsPanel
+            id="roomstats-picker"
+          />
+          <Button>
+            <Icon name="options" />
+            Filter
+          </Button>
+        </Form>
+      </Segment>
     );
   }
 
@@ -69,6 +180,7 @@ class RoomStats extends Component {
     return (
       <div>
         <h1> Room stats </h1>
+        {this.renderDateFilter()}
         {this.renderDropDown()}
         {selected.includes('Hours Booked') ? this.renderChart('Hours Booked', 'hours_booked') : ''}
         {selected.includes('Number of Room Bookings') ? this.renderChart('Number of Room Bookings', 'num_room_bookings') : ''}
