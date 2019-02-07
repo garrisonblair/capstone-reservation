ae1158f87c94e5df532146c226c2752c68447e20
Frederic Daigle <Freddydaigle@gmail.com>
2019-02-07 16:37:00 -0500

    #142 Finished implementing main functionality of card reader.  Added detailed logging.

--- card_reader/card_client.py
+++ card_reader/card_client.py
@@ -1,7 +1,36 @@
+import requests
+import os
+from dotenv import load_dotenv
 
+import log_util
+
+logger = log_util.get_logger()
+load_dotenv()
+
+HOST = os.getenv("HOST")
+DEVICE_ID = os.getenv("DEVICE_ID")
 
 while True:
 	card_content = input()
-	
-	student_id = cardcontent
-	print(card_content)
+	card_id = card_content[6:14]
+
+	logger.info("Card swiped: " + card_id)
+	try:
+		response = requests.request(
+			"POST",
+			HOST + "/card_read",
+			data={
+				"device_id": DEVICE_ID,
+				"card_id": card_id
+			})
+
+	except Exception as e:
+		logger.error(e)
+		pass
+
+	if 200 >= response.status_code < 300:
+		logger.info("Request successful")
+		# TODO: Light green LED
+	else:
+		logger.info("Request failed with status " + str(response.status_code))
+		# TODO: Light red LED
