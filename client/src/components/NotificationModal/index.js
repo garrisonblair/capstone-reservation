0822edd2430330414985eb40609eae6b7e728632
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2019-02-20 15:31:05 -0500

    #207 verify modal

--- client/src/components/NotificationModal/index.js
+++ client/src/components/NotificationModal/index.js
@@ -1,61 +1,126 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
-  Modal, Input, FormField,
+  Modal, Input, FormField, Icon, Button,
 } from 'semantic-ui-react';
 import moment from 'moment';
-import api from '../../utils/api';
+// import api from '../../utils/api';
+import sweetAlert from 'sweetalert2';
 import './NotificationModal.scss';
 
 
 class NotificationModal extends Component {
   state = {
     date: moment().format('YYYY-MM-DD'),
-    startTime: moment(new Date()).format('hh:mm'),
-    endTime: moment(new Date()).format('hh:mm'),
-    rooms: [],
+    startTime: moment(new Date()).format('HH:mm'),
+    endTime: moment(new Date()).format('HH:mm'),
+    // rooms: [],
   }
 
   componentDidMount() {
-    api.getRooms()
-      .then((r) => {
-        if (r.status === 200) {
-          this.setState({ rooms: r.data });
-        }
-      });
+    // api.getRooms()
+    //   .then((r) => {
+    //     if (r.status === 200) {
+    //       this.setState({ rooms: r.data });
+    //     }
+    //   });
   }
 
   renderRoomsCheckbox = () => {
     // const { rooms}
   }
 
+  handleOnChangeDate = (e, data) => {
+    this.setState({ date: data.value });
+  }
+
+  handleOnChangeStartTime = (e, data) => {
+    this.setState({ startTime: data.value });
+  }
+
+  handleOnChangeEndTime = (e, data) => {
+    this.setState({ endTime: data.value });
+  }
+
+  verifyForm = () => {
+    const { date, startTime, endTime } = this.state;
+    console.log();
+    if (date.length === 0) {
+      sweetAlert('Blocked', 'The date cannot be empty.', 'warning');
+      return false;
+    }
+    if (startTime.length === 0) {
+      sweetAlert('Blocked', 'Start time cannot be empty.', 'warning');
+      return false;
+    }
+    if (endTime.length === 0) {
+      sweetAlert('Blocked', 'End time cannot be empty.', 'warning');
+      return false;
+    }
+    if (!(moment(startTime, 'hh:mm').isBefore(moment(endTime, 'hh:mm')))) {
+      sweetAlert('Blocked', 'End time must be after start time.', 'warning');
+      return false;
+    }
+
+    return true;
+  }
+
+  handleSubmit = () => {
+    const { date, startTime, endTime } = this.state;
+    console.log(date);
+    console.log(startTime);
+    console.log(endTime);
+
+    if (!this.verifyForm()) {
+      // return;
+    }
+  }
+
   render() {
     const {
-      rooms, startTime, endTime, date,
+      startTime, endTime, date,
     } = this.state;
-    console.log(rooms);
     const { onClose } = this.props;
     return (
-      <Modal open onClose={onClose}>
+      <Modal open onClose={onClose} size="tiny" centered={false} id="notification-modal">
         <Modal.Header>
-          Create a notification
+          <h1>Create a notification</h1>
         </Modal.Header>
         <Modal.Content>
           <Modal.Description>
             <FormField>
-              Date:
-              <Input type="date" value={date} />
+              <Icon name="calendar alternate outline" size="big" />
+              <span className="label">Date:</span>
+              <Input
+                type="date"
+                defaultValue={date}
+                onChange={this.handleOnChangeDate}
+              />
             </FormField>
             <FormField>
-              Start time:
-              <Input type="time" size="small" icon="calendar alternate outline" value={startTime} />
+              <Icon name="clock outline" size="big" />
+              <span className="label">Start time:</span>
+              <Input
+                type="time"
+                defaultValue={startTime}
+                onChange={this.handleOnChangeStartTime}
+              />
             </FormField>
             <FormField>
-              End time:
-              <Input type="time" size="small" icon="calendar alternate" value={endTime} />
+              <Icon name="clock" size="big" />
+              <span className="label">End time:</span>
+              <Input
+                type="time"
+                defaultValue={endTime}
+                onChange={this.handleOnChangeEndTime}
+              />
             </FormField>
           </Modal.Description>
         </Modal.Content>
+        <Modal.Actions>
+          <Button color="blue" onClick={this.handleSubmit}>Submit</Button>
+          <Button onClick={onClose}>Close</Button>
+        </Modal.Actions>
       </Modal>
 
     );
