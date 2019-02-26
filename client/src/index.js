366066285e4f1f7bf268987ef380092365d1086f
Frederic Daigle <Freddydaigle@gmail.com>
2019-02-26 15:11:26 -0500

    #271 authentication issue workaround.  If the Token is invalid the UI logs out

--- client/src/index.js
+++ client/src/index.js
@@ -20,36 +20,52 @@ import GroupPrivilegeRequest from './components/Admin/GroupPrivilegeRequest';
 import Announcements from './components/Admin/Announcements';
 import Dashboard from './components/Dashboard';
 import Profile from './components/Profile';
+import api from './utils/api';
+import storage from './utils/local-storage';
 
+function renderPage() {
+  ReactDOM.render(
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/registration" component={Registration} />
+          <Route exact path="/resetPassword" component={ResetPassword} />
+          <Route exact path="/verify/:token" component={Verification} />
+          <Route exact path="/verifyReset/:token" component={ResetPasswordVerification} />
+          <Route exact path="/admin" render={() => <Admin menuType="settings" content={<Settings />} />} />
+          <Route exact path="/admin/settings" render={() => <Admin menuType="settings" content={<Settings />} />} />
+          <Route exact path="/admin/bookers" render={() => <Admin menuType="bookers" content={<Bookers />} />} />
+          <Route exact path="/admin/privileges" render={() => <Admin menuType="privileges" content={<PrivilegeCategory />} />} />
+          <Route exact path="/admin/privileges/requests" render={() => <Admin menuType="group-privilege-request" content={<GroupPrivilegeRequest />} />} />
+          <Route exact path="/admin/rooms" render={() => <Admin menuType="rooms" content={<RoomManager />} />} />
+          <Route exact path="/admin/logs" render={() => <Admin menuType="logs" content={<BookingActivity />} />} />
+          <Route exact path="/admin/stats" render={() => <Admin menuType="stats" content={<Stats />} />} />
+          <Route exact path="/admin/bookers" render={() => <Admin menuType="bookers" content={<Bookers />} />} />
+          <Route exact path="/admin/announcements" render={() => <Admin menuType="announcements" content={<Announcements />} />} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/profile" component={Profile} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>,
+    document.getElementById('root'),
+  );
+}
 
 if (settings.IS_PROD) {
   ReactGA.initialize(settings.gaTrackingID);
 }
 
-ReactDOM.render(
-  <Router>
-    <div>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/registration" component={Registration} />
-        <Route exact path="/resetPassword" component={ResetPassword} />
-        <Route exact path="/verify/:token" component={Verification} />
-        <Route exact path="/verifyReset/:token" component={ResetPasswordVerification} />
-        <Route exact path="/admin" render={() => <Admin menuType="settings" content={<Settings />} />} />
-        <Route exact path="/admin/settings" render={() => <Admin menuType="settings" content={<Settings />} />} />
-        <Route exact path="/admin/bookers" render={() => <Admin menuType="bookers" content={<Bookers />} />} />
-        <Route exact path="/admin/privileges" render={() => <Admin menuType="privileges" content={<PrivilegeCategory />} />} />
-        <Route exact path="/admin/privileges/requests" render={() => <Admin menuType="group-privilege-request" content={<GroupPrivilegeRequest />} />} />
-        <Route exact path="/admin/rooms" render={() => <Admin menuType="rooms" content={<RoomManager />} />} />
-        <Route exact path="/admin/logs" render={() => <Admin menuType="logs" content={<BookingActivity />} />} />
-        <Route exact path="/admin/stats" render={() => <Admin menuType="stats" content={<Stats />} />} />
-        <Route exact path="/admin/bookers" render={() => <Admin menuType="bookers" content={<Bookers />} />} />
-        <Route exact path="/admin/announcements" render={() => <Admin menuType="announcements" content={<Announcements />} />} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/profile" component={Profile} />
-        <Route component={NotFound} />
-      </Switch>
-    </div>
-  </Router>,
-  document.getElementById('root'),
-);
+const user = storage.getUser();
+if (user) {
+  api.getUserBookings(user.id)
+    .then(() => {
+      renderPage();
+    })
+      localStorage.removeItem('CapstoneReservationUser');
+      renderPage();
+    });
+} else {
+  renderPage();
+}
