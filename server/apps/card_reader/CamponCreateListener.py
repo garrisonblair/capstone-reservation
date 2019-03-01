41ce42ede13d4bffff79624d8aa0fd6d85ef1843
Frederic Daigle <Freddydaigle@gmail.com>
2019-02-28 19:16:50 -0500

    #208 fixed expiration time format

--- server/apps/card_reader/CamponCreateListener.py
+++ server/apps/card_reader/CamponCreateListener.py
@@ -1,6 +1,6 @@
 from apps.util.ModelObserver import ModelObserver
 from apps.util import utils
-from datetime import datetime
+from datetime import datetime, time
 
 
 from apps.system_administration.models.system_settings import SystemSettings
@@ -20,11 +20,12 @@ class CamponCreateListener(ModelObserver):
             campon.camped_on_booking.save()
             utils.log_model_change(campon, utils.CHANGE)
 
-            expiration_time = campon.camped_on_booking.get_expiration()
+            expiration_time = campon.camped_on_booking.get_expiration()  # type: time
 
             email_subject = "Your booking has been camped on"
             email_message = "Someone is camping on to your booking.\n"\
-                            "If you do not reconfirm your booking before " + str(expiration_time) + ",\n"\
+                            "If you do not reconfirm your booking before " + str(expiration_time.strftime("%H:%M:%S"))\
+                            + ",\n"\
                             "it will be cancelled."
 
             booker.send_email(email_subject, email_message)
