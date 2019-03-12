844585ec69b3ad00185b1c0521c1426a1ff7d975
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2019-03-11 21:32:42 -0400

    #230 return 400 when missing parameter

--- server/apps/booker_settings/views/email_settings.py
+++ server/apps/booker_settings/views/email_settings.py
@@ -2,6 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+
 class EmailSettings(APIView):
-    def get(self, request):
+    def patch(self, request):
+        data = request.data
+        when_booking = "when_booking"
+        if not when_booking in data:
+            return Response("'{}' is missing in request body".format(when_booking), status=status.HTTP_400_BAD_REQUEST)
         return Response(status=status.HTTP_200_OK)
