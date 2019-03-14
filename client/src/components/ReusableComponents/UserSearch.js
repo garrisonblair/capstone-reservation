ad76540bfebab4e99be0d4d8205ac2ec6f0b81f0
Frederic Daigle <Freddydaigle@gmail.com>
2019-03-13 22:06:53 -0400

    #271 removed unused imports

--- client/src/components/ReusableComponents/UserSearch.js
+++ client/src/components/ReusableComponents/UserSearch.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import {
-  Dropdown, Button, Icon,
+  Dropdown,
 } from 'semantic-ui-react';
 
 import api from '../../utils/api';
