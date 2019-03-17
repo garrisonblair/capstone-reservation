42a96e51c126c1e50afc3fa466968997bba9c608
steve locke <tap3stry@gmail.com>
2019-03-17 17:00:38 -0400

    #236 Removed unused flag from updated tests

--- server/apps/booking/tests/testRecurringBookingAPI.py
+++ server/apps/booking/tests/testRecurringBookingAPI.py
@@ -636,7 +636,6 @@ class BookingAPITest(TestCase):
 
         request = self.factory.post("booking/" + str(earliest_booking.id) + "/edit_recurring_booking",
                                     {
-                                        "edit_all_instances": True,
                                         "room": edited_room.id,
                                         "booking_start_time": str(edited_start_time)[:5],
                                         "booking_end_time": str(edited_end_time)[:5]
@@ -772,7 +771,6 @@ class BookingAPITest(TestCase):
 
         request = self.factory.post("booking/" + str(earliest_booking.id) + "/edit_recurring_booking",
                                     {
-                                        "edit_all_instances": True,
                                         "room": edited_room.id,
                                         "date": edited_date,
                                         "booking_start_time": str(edited_start_time)[:5],
@@ -912,7 +910,6 @@ class BookingAPITest(TestCase):
 
         request = self.factory.post("booking/" + str(999) + "/edit_recurring_booking",
                                     {
-                                        "edit_all_instances": True,
                                         "room": edited_room.id,
                                         "date": edited_date,
                                         "booking_start_time": str(edited_start_time)[:5],
@@ -1195,7 +1192,6 @@ class BookingAPITest(TestCase):
 
         request = self.factory.post("booking/" + str(999) + "/edit_recurring_booking",
                                     {
-                                        "edit_all_instances": True,
                                         "room": edited_room.id,
                                         "booking_start_time": str(edited_start_time)[:5],
                                         "booking_end_time": str(edited_end_time)[:5],
