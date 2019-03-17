ef7dfc4dd2ab5be4c8612c9b9a8da3e5324bbba6
xTEddie <edwardtranv1@gmail.com>
2019-03-16 21:20:55 -0400

    #271 Update urls

--- server/apps/statistics/urls.py
+++ server/apps/statistics/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from apps.statistics.views.room_statistics import RoomStatistics
+from apps.statistics.views.program_statistics import ProgramStatistics
 
 
 urlpatterns = [
-    path(r'room_statistics', RoomStatistics.as_view())
+    path(r'room_statistics', RoomStatistics.as_view()),
+    path(r'program_statistics', RoomStatistics.as_view())
 ]
