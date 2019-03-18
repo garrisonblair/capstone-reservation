541af193206b1433492e7c6282b8575203856652
steve locke <tap3stry@gmail.com>
2019-03-18 00:36:04 -0400

    #236 Updated the api view to handle changes from the updated model

--- server/apps/booking/views/recurring_booking.py
+++ server/apps/booking/views/recurring_booking.py
@@ -131,19 +131,19 @@ class RecurringBookingEdit(APIView):
 
         skip_conflicts = request.data['skip_conflicts']
 
-        if data["start_date"]:
+        if "start_date" in data:
             start_date = data["start_date"]
         else:
             start_date = None
-        if data["end_date"]:
+        if "end_date" in data:
             end_date = data["end_date"]
         else:
             end_date = None
-        if data["booking_start_time"]:
+        if "booking_start_time" in data:
             booking_start_time = data["booking_start_time"]
         else:
             booking_start_time = None
-        if data["booking_end_time"]:
+        if "booking_end_time" in data:
             booking_end_time = data["booking_end_time"]
         else:
             booking_end_time = None
@@ -158,7 +158,7 @@ class RecurringBookingEdit(APIView):
                                                                             booking_end_time,
                                                                             skip_conflicts,
                                                                             request.user)
-            if len(conflicts) > 0:
+            if conflicts is not None:
                 Response(conflicts, status=status.HTTP_409_CONFLICT)
             else:
                 return Response(status=status.HTTP_200_OK)
@@ -167,105 +167,3 @@ class RecurringBookingEdit(APIView):
                 return Response(error.message, status=status.HTTP_401_UNAUTHORIZED)
             else:
                 return Response(error.message, status=status.HTTP_400_BAD_REQUEST)
-
-        # try:
-        #     all_conflicts = []
-        #     non_conflicting_bookings = []
-        #
-        #     # Gets all bookings associated to the indicated booking
-        #     all_associated_booking_instances = associated_recurring_booking.booking_set.all()
-        #
-        #     # FInd the earliest booking in the list
-        #     earliest_booking = all_associated_booking_instances[0]
-        #     for booking in all_associated_booking_instances:
-        #         if booking.start_date < earliest_booking.start_date:
-        #             earliest_booking = booking
-        #
-        #     # Determine offset based on earlist booking
-        #     booking_offset = earliest_booking - data["start_date"]
-        #
-        #     # Get current end date and apply offset in case start date has changed
-        #     end_date = data["end_date"]
-        #     current_end_date = associated_recurring_booking.end_date
-        #     current_end_date = current_end_date + datetime.timedelta(days=booking_offset)
-        #
-        #     # Create new bookings if new end date has been extended and add to appropriate lists
-        #     while current_end_date < end_date:
-        #         # Sets the next booking in series
-        #         current_end_date = current_end_date + datetime.timedelta(days=7)
-        #         if current_end_date < end_date:
-        #             try:
-        #                 booking = Booking.objects.create_booking(
-        #                     booker=associated_recurring_booking.booker,
-        #                     group=associated_recurring_booking.group,
-        #                     room=associated_recurring_booking.room,
-        #                     date=current_end_date,
-        #                     start_time=data["booking_start_time"],
-        #                     end_time=data["booking_end_time"],
-        #                     recurring_booking=associated_recurring_booking,
-        #                     confirmed=False
-        #                 )
-        #                 non_conflicting_bookings.add(booking)
-        #             except ValidationError:
-        #                 if not skip_conflicts:
-        #                     all_conflicts.append(booking)
-        #                 else:
-        #                     continue
-        #
-        #     # Iterates through associated bookings and makes the adjustment
-        #     for associated_booking in all_associated_booking_instances:
-        #
-        #         # Modify the start and end times as needed
-        #         if "booking_start_time" in data:
-        #             associated_booking.start_time = data["booking_start_time"]
-        #         if "booking_end_time" in data:
-        #             associated_booking.end_time = data["booking_end_time"]
-        #
-        #
-        #
-        #         recurring_booking, conflicts = RecurringBooking.objects.edit_recurring_booking(
-        #             start_date=associated_booking.start_date + datetime.timedelta(days=booking_offset),
-        #             end_date=associated_booking.end_date + datetime.timedelta(days=booking_offset),
-        #             start_time=associated_booking.booking_start_time,
-        #             end_time=associated_booking.booking_end_time,
-        #             room=associated_booking.room,
-        #             group=associated_booking.group,
-        #             booker=associated_booking.booker,
-        #             skip_conflicts=associated_booking.skip_conflicts
-        #         )
-        #
-        #         if len(conflicts) != 0 and not skip_conflicts:
-        #             for conflict in conflicts:
-        #                 all_conflicts.append(conflict)
-        #         else:
-        #             non_conflicting_bookings.append(associated_booking)
-        #
-        #
-        #     # If there are no conflicts, simply make adjustments accordingly
-        #     if len(all_conflicts) == 0 or skip_conflicts:
-        #         for non_conflicting in non_conflicting_bookings:
-        #             # Make sure bookings are in range and have not ended before modifying
-        #             if non_conflicting.end_date <= data["end_date"] and \
-        #                     non_conflicting.start_date >= data["start_date"] and \
-        #                     non_conflicting.start_date >= now.date():
-        #                 non_conflicting.save()
-        #                 utils.log_model_change(booking, utils.CHANGE, request.user)
-        #             # Make sure bookings are in range and have not ended before deleting. Can't delete past bookings
-        #             elif non_conflicting.start_date >= data["start_date"] and \
-        #                     non_conflicting.start_date >= now.date():
-        #                 non_conflicting.delete()
-        #         return Response(status=status.HTTP_200_OK)
-        #     else:
-        #         # If there are any conflicts, return list of them
-        #         return Response(all_conflicts, status=status.HTTP_409_CONFLICT)
-        #
-        # except (ValidationError, PrivilegeError) as error:
-        #     if isinstance(error, PrivilegeError):
-        #         return Response(error.message, status=status.HTTP_401_UNAUTHORIZED)
-        #     elif ((error.message == "You must book for at least two consecutive weeks.") or
-        #           (error.message == "Start time can not be after End time.")):
-        #         return Response(error.message, status=status.HTTP_400_BAD_REQUEST)
-        #     else:
-        #         return Response(error.message, status=status.HTTP_409_CONFLICT)
-        #
-        # return Response(status=status.HTTP_200_OK)
