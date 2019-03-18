f450eab2433a64483f3ddf2e55c66761ad535ed9
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2019-03-18 15:41:09 -0400

    #230 add service url

---
+++ server/apps/booker_settings/views/email_settings_service.py
@@ -0,0 +1,29 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from apps.booker_settings.models.EmailSettings import EmailSettings as EmailSettingsModel
+from apps.accounts.models.User import User
+from apps.booker_settings.serializers.booker_settings import EmailSettingsSerializer
+from django.core.exceptions import ValidationError
+
+class EmailSettingsService(APIView):
+    authentication_classes = ()
+    permission_classes = ()
+    def post(self, request):
+        # data = request.data
+
+        # email_settings = EmailSettingsModel.objects.get(booker=request.user)
+
+        # try:
+        #     email_settings.update(data)
+        # except ValidationError as error:
+        #     return Response(error.message, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response('test2',status=status.HTTP_200_OK)
+
+
+    def get(self, request):
+        # email_settings = EmailSettingsModel.objects.get(booker=request.user)
+        # serializer = EmailSettingsSerializer(email_settings)
+        return Response('test1',status=status.HTTP_200_OK)
+        # return Response(serializer.data,status=status.HTTP_200_OK)
