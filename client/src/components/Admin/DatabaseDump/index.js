c6e25efe0f81b0a9fef012af456d12db41cdc3c0
xTEddie <edwardtranv1@gmail.com>
2019-03-19 16:24:27 -0400

    #200 Update component to keep track of the selected model from the dropdown

--- client/src/components/Admin/DatabaseDump/index.js
+++ client/src/components/Admin/DatabaseDump/index.js
@@ -1,13 +1,85 @@
+/* eslint-disable no-console */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
-// import api from '../../../utils/api';
-
+import { Button, Dropdown, Form, Header, Icon, Segment } from 'semantic-ui-react';
+import api from '../../../utils/api';
+import './DatabaseDump.scss';
 
 class DatabaseDump extends Component {
+
+  state = {
+    models: [],
+    selectedModel: '',
+  }
+
+  componentDidMount() {
+    this.getModels();
+  }
+
+  getModels = () => {
+    api.getCSV()
+      .then((response) => {
+        const { data: models } = response;
+        this.setState({ models });
+      });
+  }
+
+  onDropDownChange = (event, data) => {
+    this.setState({ selectedModel: data.value });
+  }
+
+  renderDropDown = () => {
+    const { models, selectedModel } = this.state;
+
+    let modelOptions = [];
+    modelOptions = models.map((model, index) => ({ key: index, value: model, text: model }));
+    return (
+      <Dropdown
+        placeholder="Select Model"
+        fluid
+        search
+        selection
+        options={modelOptions}
+        value={selectedModel}
+        onChange={this.onDropDownChange}
+      />
+    );
+  }
+
+  download = () => {
+    const { selectedModel } = this.state;
+    console.log(selectedModel);
+  }
+
+  renderCSVForm = () => {
+    return (
+      <Segment>
+        <Form>
+          <Header as="h2">
+            CSV
+          </Header>
+          <Form.Field>
+            {this.renderDropDown()}
+          </Form.Field>
+          <Form.Field>
+            <Button primary onClick={this.download}>
+              <Icon name="download" />
+              Download
+            </Button>
+          </Form.Field>
+        </Form>
+      </Segment>
+    )
+  }
+
   render() {
     return (
-      <div>
-        <h1>Database Dump</h1>
+      <div className="database-dump">
+        <Header as="h1">
+          <Icon name="database" />
+          Database Dump
+        </Header>
+        {this.renderCSVForm()}
       </div>
     )
   }
