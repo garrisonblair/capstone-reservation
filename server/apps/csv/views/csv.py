a6f7d35ce617e798f0d4c87fd0a248a22fd1de90
xTEddie <edwardtranv1@gmail.com>
2019-03-19 17:05:19 -0400

    #200 Add POST for csv endpoint to generate csv

--- server/apps/csv/views/csv.py
+++ server/apps/csv/views/csv.py
@@ -1,4 +1,6 @@
-import django.apps
+import csv
+from django.apps import apps
+from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -10,6 +12,26 @@ class CsvView(APIView):
     permission_classes = (IsAuthenticated, IsSuperUser)
 
     def get(self, request):
-        models = django.apps.apps.get_models()
+        models = apps.get_models()
         response = [model.__name__ for model in models]
         return Response(response)
+
+    def post(self, request):
+        model = ''
+        if 'model' in request.data:
+            model = request.data['model']
+        models = apps.get_models()
+        model_instance = [instance for instance in models if instance.__name__ == model][0]
+        meta = model_instance._meta
+        fields = [field.name for field in meta.fields] # CSV headers
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="{}.csv"'.format(model)
+
+        writer = csv.writer(response)
+        writer.writerow(fields)
+
+        for instance in model_instance.objects.all():
+            row = [getattr(instance, field) for field in fields]
+            writer.writerow(row)
+        return response
