035b05e70d311ebd035320f9c9684ed557c2e8bb
Garrison Blair <garrisonblair96@gmail.com>
2019-03-19 18:23:03 -0400

    #229 css fix

--- client/src/components/Mobile/Dashboard/index.js
+++ client/src/components/Mobile/Dashboard/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Accordion, Icon } from 'semantic-ui-react'
+import { Accordion, Icon } from 'semantic-ui-react';
 import withTracker from '../../HOC/withTracker';
 import UserBookings from '../../UserBookings';
 import Privileges from '../../Privileges';
