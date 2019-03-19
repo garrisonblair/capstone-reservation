9413711fd110ddd71ea1260cffbc6edb1d6c2478
xTEddie <edwardtranv1@gmail.com>
2019-03-19 15:47:46 -0400

    #200 Add basic DatabaseDump component

---
+++ client/src/components/Admin/DatabaseDump/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+// import api from '../../../utils/api';
+
+
+class DatabaseDump extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Database Dump</h1>
+      </div>
+    )
+  }
+}
+
+export default DatabaseDump;
