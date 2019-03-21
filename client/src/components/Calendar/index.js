141dd61bec9f2b240d66955c21be958132d3f90b
kemoua <kemoua@gmail.com>
2019-03-21 17:15:42 -0400

    Fix lint

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Icon, Button } from 'semantic-ui-react';
+import { Icon } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import Announcement from './Announcement';
 import Cells from './Cells';
