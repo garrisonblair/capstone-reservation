637b0a948a9344d05c776fa7995e81e47f94652c
kammy609 <kammy609@msn.com>
2019-03-27 15:51:20 -0400

    #218 Change style of color picker

--- client/src/components/UserSettings/PersonalSettings/index.js
+++ client/src/components/UserSettings/PersonalSettings/index.js
@@ -1,8 +1,9 @@
+/* eslint-disable react/jsx-curly-spacing */
 import React, { Component } from 'react';
 import {
-  Button, Table, Segment,
+  Button, Table, Segment, Input,
 } from 'semantic-ui-react';
-import { SketchPicker, Hue } from 'react-color';
+import { ChromePicker } from 'react-color';
 import sweetAlert from 'sweetalert2';
 import api from '../../../utils/api';
 import './PersonalSettings.scss';
@@ -15,6 +16,9 @@ class PersonalSettings extends Component {
     passedBookingColor: '#7F7F7F',
     isLoading: false,
     disableButton: true,
+    displayBookingColorPicker: false,
+    displayCamponColorPicker: false,
+    displayPassedBookingColorPicker: false,
   }
 
   componentDidMount() {
@@ -93,6 +97,39 @@ class PersonalSettings extends Component {
     });
   };
 
+  handleBookingColorClick = () => {
+    const {
+      displayBookingColorPicker,
+    } = this.state;
+    this.setState({ displayBookingColorPicker: !displayBookingColorPicker });
+  };
+
+  handleCampOnColorClick = () => {
+    const {
+      displayCamponColorPicker,
+    } = this.state;
+    this.setState({ displayCamponColorPicker: !displayCamponColorPicker });
+  };
+
+  handlePassedBookingColorClick = () => {
+    const {
+      displayPassedBookingColorPicker,
+    } = this.state;
+    this.setState({ displayPassedBookingColorPicker: !displayPassedBookingColorPicker });
+  };
+
+  handleBooingColorPickerClose = () => {
+    this.setState({ displayBookingColorPicker: false });
+  };
+
+  handleCampOnColorPickerClose = () => {
+    this.setState({ displayCamponColorPicker: false });
+  };
+
+  handlePassedBooingColorPickerClose = () => {
+    this.setState({ displayPassedBookingColorPicker: false });
+  };
+
   handleSaveOnClick = () => {
     const {
       bookingColor, camponColor, passedBookingColor,
@@ -135,9 +172,21 @@ class PersonalSettings extends Component {
 
   render() {
     const {
-      bookingColor, camponColor, passedBookingColor, isLoading,
-      disableButton,
+      bookingColor, camponColor, passedBookingColor,
+      isLoading, disableButton,
+      displayBookingColorPicker, displayCamponColorPicker, displayPassedBookingColorPicker
     } = this.state;
+    const popover = {
+      position: 'absolute',
+      zIndex: '2',
+    };
+    const cover = {
+      position: 'fixed',
+      top: '0px',
+      right: '0px',
+      bottom: '0px',
+      left: '0px',
+    };
     return (
       <div id="personal-settings">
         <h1>Personal Settings</h1>
@@ -145,7 +194,8 @@ class PersonalSettings extends Component {
           <Table celled collapsing>
             <Table.Header>
               <Table.Row>
-                <Table.HeaderCell>Personalization</Table.HeaderCell>
+                <Table.HeaderCell>Color title</Table.HeaderCell>
+                <Table.HeaderCell>Color code (hex)</Table.HeaderCell>
                 <Table.HeaderCell />
               </Table.Row>
             </Table.Header>
@@ -153,49 +203,106 @@ class PersonalSettings extends Component {
               <Table.Row>
                 <Table.Cell>Booking color</Table.Cell>
                 <Table.Cell collapsing>
-                  <input
-                    type="text"
-                    id="bookingColor"
-                    value={bookingColor}
+                  <Input
+                    fluid
+                    size="small"
+                    iconPosition="left"
+                    placeholder={bookingColor}
                     onChange={this.handleBookingColorChange}
                     onKeyPress={this.handleKeyPress}
                   />
-                  <SketchPicker
-                    color={this.handleBookingColorChange}
-                    onChangeComplete={this.handlePickBookingColor}
-                  />
+                </Table.Cell>
+                <Table.Cell collapsing>
+                  <Button
+                    color="grey"
+                    onClick={this.handleBookingColorClick}
+                  >
+                    Choose color
+                  </Button>
+                  { displayBookingColorPicker
+                    ? (
+                      <div style={popover}>
+                        <div
+                          role="presentation"
+                          style={cover}
+                          onClick={this.handleBooingColorPickerClose}
+                          onKeyDown={this.handleBooingColorPickerClose}
+                        />
+                        <ChromePicker
+                          color={this.handlePickBookingColor}
+                          onChangeComplete={this.handlePickBookingColor}
+                        />
+                      </div>) : null }
                 </Table.Cell>
               </Table.Row>
               <Table.Row>
                 <Table.Cell>Campon color</Table.Cell>
                 <Table.Cell collapsing>
-                  <input
-                    type="text"
-                    id="camponColor"
-                    value={camponColor}
+                  <Input
+                    fluid
+                    size="small"
+                    iconPosition="left"
+                    placeholder={camponColor}
                     onChange={this.handleCamponColorChange}
                     onKeyPress={this.handleKeyPress}
                   />
-                  <SketchPicker
-                    color={this.handleCamponColorChange}
-                    onChangeComplete={this.handlePickCampOnColor}
-                  />
+                </Table.Cell>
+                <Table.Cell collapsing>
+                  <Button
+                    color="grey"
+                    onClick={this.handleCampOnColorClick}
+                  >
+                    Choose color
+                  </Button>
+                  { displayCamponColorPicker
+                    ? (
+                      <div style={popover}>
+                        <div
+                          role="presentation"
+                          style={cover}
+                          onClick={this.handleCampOnColorPickerClose}
+                          onKeyDown={this.handleCampOnColorPickerClose}
+                        />
+                        <ChromePicker
+                          color={this.handlePickCampOnColor}
+                          onChangeComplete={this.handlePickCampOnColor}
+                        />
+                      </div>) : null }
                 </Table.Cell>
               </Table.Row>
               <Table.Row>
                 <Table.Cell>Passed booking color</Table.Cell>
                 <Table.Cell collapsing>
-                  <input
-                    type="text"
-                    id="passedBookingColor"
-                    value={passedBookingColor}
+                  <Input
+                    fluid
+                    size="small"
+                    iconPosition="left"
+                    placeholder={passedBookingColor}
                     onChange={this.handlePassedBookingColorChange}
                     onKeyPress={this.handleKeyPress}
                   />
-                  <SketchPicker
-                    color={this.handlePassedBookingColorChange}
-                    onChangeComplete={this.handlePickPassedBookingColor}
-                  />
+                </Table.Cell>
+                <Table.Cell collapsing>
+                  <Button
+                    color="grey"
+                    onClick={this.handlePassedBookingColorClick}
+                  >
+                    Choose color
+                  </Button>
+                  { displayPassedBookingColorPicker
+                    ? (
+                      <div style={popover}>
+                        <div
+                          role="presentation"
+                          style={cover}
+                          onClick={this.handlePassedBooingColorPickerClose}
+                          onKeyDown={this.handlePassedBooingColorPickerClose}
+                        />
+                        <ChromePicker
+                          color={this.handlePickPassedBookingColor}
+                          onChangeComplete={this.handlePickPassedBookingColor}
+                        />
+                      </div>) : null }
                 </Table.Cell>
               </Table.Row>
             </Table.Body>
